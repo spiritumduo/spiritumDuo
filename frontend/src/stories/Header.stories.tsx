59096ccb11af4823f479aacdb146765ea64a41ea
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-10-18 19:36:12 +0100

    Modified header

--- frontend/src/stories/Header.stories.tsx
+++ frontend/src/stories/Header.stories.tsx
@@ -4,16 +4,10 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Header } from './Header';
 
 export default {
-  title: 'Example/Header',
+  title: 'Header',
   component: Header,
 } as ComponentMeta<typeof Header>;
 
 const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  user: {},
-};
-
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {};
+export const Standard = Template.bind({});
