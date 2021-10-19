a820f24dd40d47006c97e7e2ac2199f9555a547e
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-10-19 12:45:33 +0100

    Routing: links and search
    Could do with going over the search bar at a later date. It works as a proof of concept but I'm not sure it's the best way

--- frontend/src/stories/Header.stories.tsx
+++ frontend/src/stories/Header.stories.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
 import { Header } from './Header';
+import StoryRouter from 'storybook-react-router';
 
 export default {
   title: 'Header',
   component: Header,
+  decorators: [ StoryRouter() ],
 } as ComponentMeta<typeof Header>;
+
+
 
 const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
 
--- frontend/src/stories/Header.tsx
+++ frontend/src/stories/Header.tsx
@@ -1,39 +1,44 @@
-import React from 'react';
+import { useState } from 'react';
+import { Link, Redirect } from "react-router-dom";
 
-import { Button } from './Button';
 import './header.css';
 import { PathwaySelector } from './PathwaySelector';
+import { useHistory } from 'react-router';
 
 interface HeaderProps {
 	user?: {};
 }
 
-export const Header = ({user}: HeaderProps) => (
-	<nav className="navbar navbar-expand-lg navbar-light bg-light">
-	<div className="container-fluid">
-		<form className="d-flex">
-			<input className="form-control me-2" type="search" placeholder="Hospital number" aria-label="Hospital number"/>
-		</form>
-		<div className="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul className="navbar-nav mb-2">
-				<li className="nav-item">
-					<a className="nav-link active" aria-current="page" href="#">Home</a>
-				</li>
-				<li className="nav-item">
-					<a className="nav-link" href="#">Triage</a>
-				</li>
-				<li className="nav-item">
-					<a className="nav-link" href="#">Clinic</a>
-				</li>
-				<li className="nav-item">
-					<a className="nav-link" href="#">MDT</a>
-				</li>
-				<li className="nav-item">
-					<a className="nav-link" href="#">Refer</a>
-				</li>
-			</ul>
-		</div>
-		<PathwaySelector options={["Lung cancer", "Bronchieactasis"]} onItemSelect={(data:string)=>console.log(data)}/>
-	</div>
-</nav>
-);
+export const Header = ({user}: HeaderProps) => {
+	const history=useHistory();
+	return(
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<div className="container-fluid">
+				<form className="d-flex" onSubmit={e=>{e.preventDefault(); history.push("/patient/"+e.target[0].value);}}>
+					<input className="form-control me-2" type="search" name="hospitalNumberSearch" placeholder="Hospital number" aria-label="Hospital number" />
+				</form>
+				
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav mb-2">
+						<li className="nav-item">
+							<Link className="nav-link active" to={"/page/Home"}>Home</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link active" to={"/page/Triage"}>Triage</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link active" to={"/page/Clinic"}>Clinic</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link active" to={"/page/MDT"}>MDT</Link>
+						</li>
+						<li className="nav-item">
+							<Link className="nav-link active" to={"/page/Refer"}>Refer</Link>
+						</li>
+					</ul>
+				</div>
+				<PathwaySelector options={["Lung cancer", "Bronchieactasis"]} currentOption="Lung cancer" onItemSelect={(data:string)=>console.log(data)}/>
+			</div>
+		</nav>
+	);
+};
