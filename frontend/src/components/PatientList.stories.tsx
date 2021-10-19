f0f39aaaed5fa92627934b8bff75bdae56b1c090
Nick Ives <nickives@gmail.com>
2021-10-19 16:01:53 +0100

    Patient list with pagination

--- frontend/src/components/PatientList.stories.tsx
+++ frontend/src/components/PatientList.stories.tsx
@@ -1,7 +1,7 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import PatientList  from './PatientList';
+import { ComponentStory, Story, ComponentMeta } from '@storybook/react';
+import PatientList, { PatientListProps, PatientListUpdateFn }  from './PatientList';
 import StoryRouter from 'storybook-react-router';
-import Patient from '../../lib/Patient';
+import Patient from '../types/Patient';
 
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -12,7 +12,7 @@ export default {
 } as ComponentMeta<typeof PatientList>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof PatientList> = (args) => <PatientList {...args} />;
+const Template: ComponentStory<typeof PatientList> = (args: PatientListProps) => <PatientList {...args} />;
 
 export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
@@ -23,9 +23,20 @@ const patient = {
 };
 
 for (let i = 0; i < 50; ++i) {
-    patientArray.push(patient);
+    const newPatient = {
+        patientId: patient.patientId + `-${i + 1}`,
+        name: patient.name + ` ${i + 1}`
+    }
+    patientArray.push(newPatient);
+}
+
+const updateFn = function (offset: number, limit: number): PatientListUpdateFn {
+    const data: Patient[] = patientArray.slice(offset, limit);
+    return { data: data, totalCount: patientArray.length};
 }
 
 Default.args = {
-    patients: patientArray
+    patients: patientArray,
+    pageLimit: 10,
+    updateData: updateFn
 };
--- frontend/src/components/PatientList.tsx
+++ frontend/src/components/PatientList.tsx
@@ -1,26 +1,97 @@
+import React from 'react'
 import './patientlist.css';
-import { Link } from "react-router-dom";
 import { PatientLink } from "./Link";
 import Patient from "../types/Patient";
+import ReactPaginate from 'react-paginate';
 
-interface PatientListProps {
+export interface PatientListProps {
   /**
-   * Patient ID
+   * Number of Patients per page
    */
-  patients: Array<Patient>;
+  pageLimit: number;
+  /**
+   * Total number of patients to in data source
+   */
+  totalCount: number;
+  /**
+   * Function to update patient data
+   */
+  updateData: (offset: number, limit: number) => { data: Patient[], totalCount: number };
 }
 
 /**
- * Link to individual Patient 
+ * List of patients with pagination
+ * 
+ * This is a class because it has to maintain the pagination state
  */
-const PatientList = ({ patients }: PatientListProps) => {
-  return (
-    <ul className="patient-list">
-      {
-        patients.map( p => ( <li> <PatientLink patientId={p.patientId} name={p.name} /> </li>))
-      }
-    </ul>
-  );
+class PatientList extends React.Component {
+
+  state: {
+    data: Patient[];
+    offset: number;
+    pageCount: number;
+  }
+
+  props!: PatientListProps; // super(props) will init this
+
+  constructor(props: PatientListProps) {
+    super(props);
+    const response = props.updateData(0, props.pageLimit);
+    const pageCount = Math.ceil(response.totalCount / props.pageLimit);
+    this.state = {
+      data: response.data,
+      offset: 0,
+      pageCount: pageCount,
+    }
+  }
+
+  handlePageClick = (e) => {
+    const offset = e.selected * this.props.pageLimit;
+    const limit = (e.selected + 1) * this.props.pageLimit;
+    const response = this.props.updateData(offset, limit);
+    const pageCount = Math.ceil(response.totalCount / this.props.pageLimit);
+
+    this.setState({ 
+      offset: offset,
+      data:response.data,
+      pageCount: pageCount
+    });
+
+    this.render();
+  }
+
+  render() {
+    return (
+      <div className="patient-list">
+        <ul className="patient-list">
+          {
+            this.state.data.map( p => (
+              <li key={p.patientId}> <PatientLink patientId={p.patientId} name={p.name} /> </li>
+            ))
+          }
+        </ul>
+
+        <ReactPaginate
+            previousLabel={'previous'}
+            nextLabel={'next'}
+            breakLabel={'...'}
+            breakClassName={'break-me'}
+            pageCount={this.state.pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={this.handlePageClick}
+            containerClassName={'pagination'}
+            activeClassName={'active'}
+            pageClassName={'page-item'}
+            previousClassName={'page-item'}
+            nextClassName={'page-item'}
+            previousLinkClassName='page-link'
+            nextLinkClassName='page-link'
+            pageLinkClassName='page-link'
+        />
+      </div>
+    );
+  }
 };
 
 export default PatientList;
