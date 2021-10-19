5ad6d6ae567635a951a05b967dca72832b0e331b
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-10-19 12:46:35 +0100

    Updated descriptors for story, used router DOM for links, added currentOption parameter

--- frontend/src/stories/PathwaySelector.stories.tsx
+++ frontend/src/stories/PathwaySelector.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
+import StoryRouter from 'storybook-react-router';
 import { PathwaySelector } from './PathwaySelector';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -11,9 +11,16 @@ export default {
   argTypes: {
     options: { 
       name:"Options",
-      type:{name:"array", required:true}
+      type:{name:"array", required:true},
+      description:"The pathways the user may select between"
+    },
+    currentOption: { 
+      name:"Current option",
+      type:{name:"string", required:true},
+      description:"The currently selected pathway"
     },
   },
+  decorators: [ StoryRouter() ],
 } as ComponentMeta<typeof PathwaySelector>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -23,4 +30,5 @@ export const Standard = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Standard.args = {
   options: ["Lung cancer", "Bronchieactasis"],
+  currentOption:"Lung cancer"
 };
