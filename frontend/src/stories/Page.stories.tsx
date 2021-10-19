63ede1d9d096c2b199f85a3ce80f1b9535ab1b04
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-10-19 18:53:01 +0100

    Added router as decorator

--- frontend/src/stories/Page.stories.tsx
+++ frontend/src/stories/Page.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import StoryRouter from 'storybook-react-router';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Page } from './Page';
@@ -6,6 +7,7 @@ import { Page } from './Page';
 export default {
   title: 'Example/Page',
   component: Page,
+  decorators: [ StoryRouter() ],
 } as ComponentMeta<typeof Page>;
 
 const Template: ComponentStory<typeof Page> = (args) => <Page {...args} />;
