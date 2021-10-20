795d7b9dfdd28936747a57af9c675aa0e786ccca
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-10-20 14:38:02 +0100

    fixed import

--- frontend/src/components/Header.stories.tsx
+++ frontend/src/components/Header.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Header } from './Header';
+import Header from './Header';
 import StoryRouter from 'storybook-react-router';
 
 export default {
