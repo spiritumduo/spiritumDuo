56e64a19aeb5d9e662af67832e8855bb0f0e1553
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-10-20 09:25:19 +0100

    changed to form element, added link for register btn

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 interface LoginPageProps {
 
@@ -10,7 +11,7 @@ export const LoginPage = ({ }: LoginPageProps) => (
 			<div className="container py-5 h-100">
 				<div className="row d-flex justify-content-center align-items-center h-100">
 					<div className="card shadow-2-strong col-12 col-md-8 col-lg-6 col-xl-5">
-						<div className="card-body p-5">
+						<form className="card-body p-5" action="/handleLogin" method="POST">
 							<div className="form-group mb-2">
 								<label>Please enter credentials below to access Spiritum Duo</label>
 							</div>
@@ -18,20 +19,20 @@ export const LoginPage = ({ }: LoginPageProps) => (
 							<div className="form-group row mb-2">
 								<label className="col-sm-3 col-form-label">Username</label>
 								<div className="col-sm-9">
-								<input type="text" className="form-control" placeholder="john.doe" />
+								<input type="text" name="usernameTxt" className="form-control" placeholder="john.doe" />
 								</div>
 							</div>
 
 							<div className="form-group row mb-4">
 								<label className="col-sm-3 col-form-label">Password</label>
 								<div className="col-sm-9">
-								<input type="password" className="form-control" placeholder="********" />
+								<input type="password" name="passwordTxt" className="form-control" placeholder="********" />
 								</div>
 							</div>
 							
-							<button type="submit" className="btn btn-outline-secondary w-25 float-end ms-1">Login</button>
-							<button type="submit" className="btn btn-outline-secondary w-25 float-end">Register</button>
-						</div>
+							<button type="submit" name="loginBtn" className="btn btn-outline-secondary w-25 float-end ms-1">Login</button>
+							<Link to="/register" className="btn btn-outline-secondary w-25 float-end">Register</Link>
+						</form>
 					</div>
 				</div>
 			</div>
