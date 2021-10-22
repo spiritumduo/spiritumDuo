fdb0681ff35445a767b6c054143fcf04c1fcaff3
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-10-22 22:31:51 +0100

    removed initial django project files

--- backend/djangoProject/api/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .api import patientViewSet
-
-router = routers.DefaultRouter()
-router.register('patient', patientViewSet, 'patient')
-
-urlpatterns = router.urls
--- backend/djangoProject/handler/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HandlerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'handler'
