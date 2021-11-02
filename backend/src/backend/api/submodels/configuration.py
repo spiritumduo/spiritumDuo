00f69b96e9ad7bfbd9a833d5631fced308656a3a
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-02 16:38:18 +0000

    New configuration model

--- backend/src/backend/api/submodels/configuration.py
+++ backend/src/backend/api/submodels/configuration.py
@@ -1,34 +1,52 @@
-# from django.db import models
-# from dataclasses import dataclass
+from django.db import models
 
-# class config_orm(models.Model):
-#     hospitalNumberName=models.TextField()
-#     hospitalNumberRegex=models.TextField()
-#     nationalPatientNumberName=models.TextField()
-#     nationalPatientNumberRegex=models.TextField()
+class configuration_orm(models.Model):
+    hospital_number_name=models.TextField()
+    hospital_number_regex=models.TextField()
+    national_patient_number_name=models.TextField()
+    national_patient_number_regex=models.TextField()
 
-# @dataclass
-# class ConfigModel:
-#     hospitalNumberName:str
-#     hospitalNumberRegex:str
-#     nationalPatientNumberName:str
-#     nationalPatientNumberRegex:str
-#     _orm: config_orm = config_orm()
+class ConfigurationInterface:
+    def __init__(self, hospitalNumberName:str, hospitalNumberRegex:str, nationalPatientNumberName:str, nationalPatientNumberRegex:str):
+        self.hospitalNumberName=hospitalNumberName
+        self.hospitalNumberRegex=hospitalNumberRegex
+        self.nationalPatientNumberName=nationalPatientNumberName
+        self.nationalPatientNumberRegex=nationalPatientNumberRegex
+
+class ConfigurationModel:
+    def __init__(self, hospitalNumberName:str, hospitalNumberRegex:str, nationalPatientNumberName:str, nationalPatientNumberRegex:str):
+        self.hospitalNumberName=hospitalNumberName
+        self.hospitalNumberRegex=hospitalNumberRegex
+        self.nationalPatientNumberName=nationalPatientNumberName
+        self.nationalPatientNumberRegex=nationalPatientNumberRegex
+        self._orm: configuration_orm = configuration_orm()
     
-#     @classmethod
-#     def read(cls):
-#         try:
-#             return config_orm.objects.all()
-#         except (config_orm.DoesNotExist):
-#             return False
+    @classmethod
+    def read(cls):
+        try:
+            returnData=configuration_orm.objects.filter()
+            return ConfigurationInterface(
+                hospitalNumberName=returnData.hospitalNumberName,
+                hospitalNumberRegex=returnData.hospitalNumberRegex,
+                nationalPatientNumberName=returnData.nationalPatientNumberName,
+                nationalPatientNumberRegex=returnData.nationalPatientNumberRegex,
+            )
+        except (configuration_orm.DoesNotExist):
+            return False
 
-#     def save(self):
-#         self._orm.hospitalNumberName=self.hospitalNumberName
-#         self._orm.hospitalNumberRegex=self.hospitalNumberRegex
-#         self._orm.nationalPatientNumberName=self.nationalPatientNumberName
-#         self._orm.nationalPatientNumberRegex=self.nationalPatientNumberRegex
+    def delete(self):
+        self._orm.delete()
         
-#         self._orm.save()
-        
-#     def delete(self):
-#         self._orm.delete()
+    def save(self):
+        self._orm.hospital_number_name=self.hospital_number_name
+        self._orm.hospital_number_regex=self.hospital_number_regex
+        self._orm.national_patient_number_name=self.national_patient_number_name
+        self._orm.national_patient_number_regex=self.national_patient_number_rege
+        self._orm.save()
+
+        return ConfigurationInterface(
+            hospital_number_name=self._orm.hospital_number_name,
+            hospital_number_regex=self._orm.hospital_number_regex,
+            national_patient_number_name=self._orm.national_patient_number_name,
+            national_patient_number_regex=self._orm.national_patient_number_regex,
+        )
