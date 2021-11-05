1601d9561c8078d081d6eeb8eff37011ad574829
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-05 15:29:04 +0000

    added dp model

---
+++ backend/src/api/models/DecisionPoint.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from .Patient import patient_orm
+from .UserProfile import SdUser
+# Database table model
+class decisionpoint_orm(models.Model):
+    patient=models.ForeignKey(
+        to=patient_orm,
+        on_delete=models.CASCADE
+    )
+    clinician=models.ForeignKey(
+        to=SdUser,
+        on_delete=models.CASCADE
+    )
+    type=models.TextField()
+    added_at=models.DateTimeField()
+    updated_at=models.DateTimeField()
+    clinic_history=models.TextField()
+    comorbidities=models.TextField()
