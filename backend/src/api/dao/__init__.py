b60d0784551722c88b30c504185b16ff95b60470
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-08 10:55:14 +0000

    Test result DAO

--- backend/src/api/dao/__init__.py
+++ backend/src/api/dao/__init__.py
@@ -3,3 +3,4 @@ from .ConfigurationDAO import ConfigurationDAO
 from .PatientDAO import PatientDAO
 from .RoleDAO import RoleDAO
 from .DecisionPointDAO import DecisionPointDAO
+from .TestResultDAO import TestResultDAO
