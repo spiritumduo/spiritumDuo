a3af52cee06c8a24f7f2ecc923cdc4a561051479
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-09 16:03:38 +0000

    Brought names inline with GraphQL, better searching

--- backend/src/api/dao/TestResultDAO.py
+++ backend/src/api/dao/TestResultDAO.py
@@ -1,47 +1,34 @@
 from datetime import datetime
 from typing import Iterable
 
-from api.models.TestResult import testresult_orm
-
-# Interface between database and GraphQL
-class _interface:
-    def __init__(self, id:int, patient:int, addedAt:datetime, description:str, mediaUrls:str):
-        self.id=id
-        self.patient=patient
-        self.addedAt=addedAt
-        self.description=description
-        self.mediaUrls=mediaUrls
+from api.models import testresult_orm
 
 # DAO object
 class TestResultDAO:
-    def __init__(self, id:int=None, patient:int=None, addedAt:datetime=None, description:str=None, mediaUrls:str=None):
+    def __init__(self, id:int=None, patient:int=None, added_at:datetime=None, description:str=None, media_urls:str=None):
         self.id=id
         self.patient=patient
-        self.addedAt=addedAt
+        self.added_at=added_at
         self.description=description
-        self.mediaUrls=mediaUrls
-        self._orm: testresult_orm = None
+        self.media_urls=media_urls
+        self._orm: testresult_orm = testresult_orm()
 
-        if not self._orm:
-            self._orm = testresult_orm()
-
-        if self.id:
-            self._orm.id=self.id
-
-        self._orm.patient=self.patient
-        self._orm.added_at=self.addedAt
-        self._orm.description=self.description
-        self._orm.media_urls=self.mediaUrls
-        self._orm.save()
-        self.id=self._orm.id
+        if id:
+            self._orm.id=id
+            self._orm.patient=self.patient
+            self._orm.added_at=self.added_at
+            self._orm.description=self.description
+            self._orm.media_urls=self.media_urls
     
     @classmethod
-    def read(cls, id:int=None):
+    def read(cls, id:int=None, patientId:int=None):
         try:
-            if not id:
-                returnData=testresult_orm.objects.all()
-            else:
+            if id:
                 returnData=testresult_orm.objects.get(id=id)
+            elif patientId:
+                returnData=testresult_orm.objects.filter(patient=patientId)
+            else:
+                returnData=testresult_orm.objects.all()
             returnList=[]
             
             if isinstance(returnData, Iterable):
@@ -50,9 +37,9 @@ class TestResultDAO:
                         cls(
                             id=row.id,
                             patient=row.patient,
-                            addedAt=row.added_at,
+                            added_at=row.added_at,
                             description=row.description,
-                            mediaUrls=row.media_urls,
+                            media_urls=row.media_urls,
                         )
                     )
                 return returnList
@@ -60,9 +47,9 @@ class TestResultDAO:
                 return cls(
                     id=returnData.id,
                     patient=returnData.patient,
-                    addedAt=returnData.added_at,
+                    added_at=returnData.added_at,
                     description=returnData.description,
-                    mediaUrls=returnData.media_urls,
+                    media_urls=returnData.media_urls,
                 )
         except (testresult_orm.DoesNotExist):
             return False
@@ -72,15 +59,8 @@ class TestResultDAO:
         
     def save(self):
         self._orm.patient=self.patient
-        self._orm.added_at=self.addedAt
+        self._orm.added_at=self.added_at
         self._orm.description=self.description
-        self._orm.media_urls=self.mediaUrls
+        self._orm.media_urls=self.media_urls
         self._orm.save()
-
-        return _interface(
-            id=self._orm.id,
-            patient=self._orm.patient,
-            addedAt=self._orm.added_at,
-            description=self._orm.description,
-            mediaUrls=self._orm.media_urls,
-        )
+        self.id=self._orm.id
