d6473882101aac4c29aad9daee7200a9f2d506f3
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-09 11:14:45 +0000

    Updated root GraphQL schema

--- backend/src/api/schema.py
+++ backend/src/api/schema.py
@@ -3,6 +3,7 @@ from .graphql.patient.schema import PatientQueries, PatientMutations
 from .graphql.configuration.schema import ConfigurationQueries, ConfigurationMutations
 from .graphql.role.schema import RoleQueries, RoleMutations
 from .graphql.decisionpoint.schema import DecisionPointMutations, DecisionPointQueries
+from .graphql.user.schema import UserMutations, UserQueries
 
 from .graphql.login.schema import LoginMutations
 
@@ -10,13 +11,15 @@ class Queries(
     PatientQueries,
     ConfigurationQueries,
     RoleQueries,
-    DecisionPointQueries
+    DecisionPointQueries,
+    UserQueries
 ):pass
 class Mutations(
     PatientMutations,
     ConfigurationMutations,
     RoleMutations,
     DecisionPointMutations,
+    UserMutations,
 
     LoginMutations
 ):pass
