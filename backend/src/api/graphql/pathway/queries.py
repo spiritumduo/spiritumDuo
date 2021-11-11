95099270828f6eb1a4e5415266fc6dacf4f37901
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-11 12:58:20 +0000

    Fixes

--- backend/src/api/graphql/pathway/queries.py
+++ backend/src/api/graphql/pathway/queries.py
@@ -6,12 +6,8 @@ class _PathwayQueries(graphene.ObjectType):
     def _resolve_pathways(root, info):  # Gets all data
         return PathwayDAO.read()
     def _resolve_pathway_search(root, info, id=None, name=None): # Gets specified data only
-
-    def _resolve_pathway_search(root, info, id, name):  # Gets specified data only
         return PathwayDAO.read(id=id, name=name)
-
     def _resolve_patient_pathways(root, info, patientId=None):
         return PathwayDAO.readRelations(patientId=patientId)
-
     def _resolve_patients_for_pathway(root, info, pathwayId=None):
         return PathwayDAO.readRelations(pathwayId=pathwayId)
