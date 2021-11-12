4fc37e1531e4256a49ded888ea4debc41a0aca64
Nicholas Ives <nickives@gmail.com>
2021-11-12 15:57:22 +0000

    Models for Patients on Pathways

--- backend/src/api/models/PatientPathwayInstances.py
+++ backend/src/api/models/PatientPathwayInstances.py
@@ -5,8 +5,9 @@ from .DecisionPoint import DecisionPoint
 class PatientPathwayInstances(models.Model):
     patient = models.ForeignKey('api.Patient', on_delete=models.RESTRICT)
     pathway = models.ForeignKey('api.Pathway', on_delete=models.RESTRICT)
-    isDischarged = models.BooleanField(default=False)
-    awaitingDecisionType = models.CharField(
+    is_discharged = models.BooleanField(default=False)
+    awaiting_decision_type = models.CharField(
         max_length=150,
-        choices=DecisionPoint.DecisionPointType.choices
+        choices=DecisionPoint.DecisionPointType.choices,
+        default=DecisionPoint.DecisionPointType.choices.TRIAGE
     )
