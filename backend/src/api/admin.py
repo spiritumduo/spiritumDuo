71ab5b35cd2ae1858c68cfb78cc396e99c61357c
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-17 20:33:19 +0000

    Updated imports

--- backend/src/api/admin.py
+++ backend/src/api/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
 
-# from api.submodels.patient import patient_orm
-# from api.dao.UserDAO import User
-# from backend.api.submodels.user import user_orm;
-# from backend.api.submodels.role import role_orm;
-# from backend.api.submodels.configuration import configuration_orm;
-# from backend.api.submodels.decisionpoint import decision_orm;
-
-# admin.site.register(patient_orm)
-# admin.site.register(User)
-# admin.site.register(user_orm)
-# admin.site.register(role_orm)
-# admin.site.register(configuration_orm)
-# admin.site.register(decision_orm)
+from api.models import Configuration, DecisionPoint, Pathway, Patient, PatientPathwayInstances, Role, SdUser, TestResult, UserProfile
