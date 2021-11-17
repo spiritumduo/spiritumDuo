a05d2abeadb725923f8d87b63bd4021fc5e1a995
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-17 20:33:31 +0000

    Removed old DAOs

--- backend/src/api/dao/__init__.py
+++
@@ -1,7 +0,0 @@
-from .UserDAO import UserDAO
-from .ConfigurationDAO import ConfigurationDAO
-from .PatientDAO import PatientDAO
-from .RoleDAO import RoleDAO
-from .DecisionPointDAO import DecisionPointDAO
-from .TestResultDAO import TestResultDAO
-from .PathwayDAO import PathwayDAO
