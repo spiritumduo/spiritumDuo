558ef338a0bb3f1cfcb9f4f27458b984861cb4a4
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-19 10:40:41 +0000

    Ammended root schema

--- backend/src/api/gql/schema.py
+++ backend/src/api/gql/schema.py
@@ -1,9 +1,10 @@
 from ariadne import make_executable_schema, snake_case_fallback_resolvers
 
-from .types import type_defs_list as data_type_defs_list
+from .types import type_defs_list as data_type_defs_list, object_types_list
 from .scalars import type_defs as scalar_type_defs, type_list as scalar_type_list
 from .query import type_list as query_type_list, type_defs_list as query_type_defs_list
 from .mutation import type_list as mutation_type_list, type_defs_list as mutation_type_defs_list
+
 
 schema = make_executable_schema(
     [
@@ -17,5 +18,6 @@ schema = make_executable_schema(
         *query_type_list,           # many different query types
         *mutation_type_list         # many different mutation types
     ],
+    *object_types_list,
     snake_case_fallback_resolvers   # this just makes returning data easier (snakecase+camelCase conversions)
 )
