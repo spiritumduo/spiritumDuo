ef78096046aa0dba224b53732420566e2113a680
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-19 12:57:50 +0000

    Removed configuration data entries

--- backend/src/api/gql/types/configuration.py
+++
@@ -1,9 +0,0 @@
-from ariadne import gql
-type_defs=gql("""
-    type Configuration {
-        hospitalNumberName: String!
-        hospitalNumberRegex: String!
-        nationalPatientNumberName: String!
-        nationalPatientNumberRegex: String!
-    }
-""")
