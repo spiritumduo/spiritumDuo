72ec40c04e4ed931225a46688cd0c4a09141b89b
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-23 19:41:15 +0000

    Schema updates

--- backend/src/api/models/TestResult.py
+++ backend/src/api/models/TestResult.py
@@ -1,11 +1,16 @@
 from django.db import models
 from .Patient import Patient
+from .PatientPathwayInstance import PatientPathwayInstance
 
 class TestResult(models.Model):
     patient=models.ForeignKey(
         to=Patient,
         on_delete=models.CASCADE
     )
-    added_at=models.DateTimeField()
+    pathway_instance=models.ForeignKey(
+        to=PatientPathwayInstance,
+        on_delete=models.CASCADE
+    )
+    added_at=models.DateTimeField(auto_now=True)
     description=models.TextField()
     media_urls=models.TextField()
