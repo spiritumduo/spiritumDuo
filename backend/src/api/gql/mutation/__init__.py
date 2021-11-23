a9ddb023d5eb8ad4b72ffdb307e58a1221a9c8e6
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-23 19:42:09 +0000

    Added login mutation

--- backend/src/api/gql/mutation/__init__.py
+++ backend/src/api/gql/mutation/__init__.py
@@ -5,5 +5,6 @@ from .create_patient_pathway_instance import mutation
 from .create_patient import mutation
 from .create_user import mutation
 from .create_decision_point import mutation
+from .login import mutation
 
 type_list=[mutation]
