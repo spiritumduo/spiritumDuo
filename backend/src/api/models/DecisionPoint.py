36984663f49502cf356c7a2229e6d2a0b808f213
Nicholas Ives <nickives@gmail.com>
2021-11-24 12:09:55 +0000

    Added MDT, Ad-hoc, and follow up to DecisionPointType with i18n

--- backend/src/api/models/DecisionPoint.py
+++ backend/src/api/models/DecisionPoint.py
@@ -1,13 +1,16 @@
 from django.db import models
-from enum import Enum
+from django.utils.translation import gettext_lazy as _
 from .Patient import Patient
 from .Pathway import Pathway
 from .UserProfile import SdUser
 
 class DecisionPoint(models.Model):
     class DecisionPointType(models.TextChoices):
-        TRIAGE="TRIAGE"
-        CLINIC="CLINIC"
+        TRIAGE = "TRIAGE", _("Triage")
+        CLINIC = "CLINIC", _("Clinic")
+        MDT = "MDT", _("MDT")
+        AD_HOC = "AD_HOC", _("Ad-Hoc")
+        FOLLOW_UP = "FOLLOW_UP", _("Follow up")
     patient=models.ForeignKey(Patient, on_delete=models.CASCADE)
     clinician=models.ForeignKey(SdUser, on_delete=models.CASCADE)
     pathway=models.ForeignKey(Pathway, on_delete=models.CASCADE)
