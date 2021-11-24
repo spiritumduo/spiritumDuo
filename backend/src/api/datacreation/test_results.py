f3f3a57cdce4649f79df3a8955dd513f7191bdc2
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-24 11:09:18 +0000

    unfinished: test result media url correction

--- backend/src/api/datacreation/test_results.py
+++ backend/src/api/datacreation/test_results.py
@@ -1,6 +1,6 @@
 from asgiref.sync import sync_to_async
-from api.models import TestResult
-from datetime import date
+from api.models import TestResult, TestResultMedia
+from typing import List
 from api.models import Patient, PatientPathwayInstance
 
 @sync_to_async
@@ -8,16 +8,21 @@ def CreateTestResult(
     patient:Patient=None,
     pathway_instance:PatientPathwayInstance=None,
     description:str=None,
-    media_urls:str=None
+    media_urls:List[str]=None
 ):
     try:
         testResult=TestResult(
             patient=patient,
             pathway_instance=pathway_instance,
             description=description,
-            media_urls=media_urls
         )
         testResult.save()
+        for resource in media_urls:
+            resource=TestResultMedia(
+                test_result=testResult,
+                resource=resource
+            )
+            resource.save()
         return testResult
     except Exception as e:
         print(e)
--- backend/src/api/gql/types/testresult.py
+++ backend/src/api/gql/types/testresult.py
@@ -1,6 +1,6 @@
 from ariadne.objects import ObjectType
 from channels.db import database_sync_to_async
-from api.models import PatientPathwayInstance, TestResult
+from api.models import PatientPathwayInstance
 
 TestResultObjectType=ObjectType("TestResult")
 
@@ -9,15 +9,6 @@ async def resolve_instance_pathway_instance(obj=None, *_, filter=None):
     idRelated = await database_sync_to_async(PatientPathwayInstance.objects.get)(id=obj.pathway_instance_id)
     return idRelated
 
-## WARNING
-## It is abolutely necessary this gets resolved fully
-## and properly avoiding the use of string operations.
-## We can probably store it as a JSON array or something
-## Joe
-
 @TestResultObjectType.field("mediaUrls")
 async def resolve_test_result_media_urls(obj=None, *_):
-    if not obj.media_urls:
-        return None
-
-    return obj.media_urls.strip('[]').split(", ") 
+    return ["test1", "test2"]
