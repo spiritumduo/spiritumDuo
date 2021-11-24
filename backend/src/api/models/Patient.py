398f59144867885c8276e7cbf4185411f09acdca
Nicholas Ives <nickives@gmail.com>
2021-11-24 12:09:02 +0000

    Reverted identifier fields back to charfields

--- backend/src/api/models/Patient.py
+++ backend/src/api/models/Patient.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Patient(models.Model):
     id=models.BigAutoField(primary_key=True)
-    hospital_number = models.IntegerField(unique=True)
-    national_number = models.IntegerField()
+    hospital_number = models.CharField(unique=True, max_length=50)
+    national_number = models.CharField(unique=True, max_length=50)
     communication_method = models.TextField()
     first_name = models.TextField()
     last_name = models.TextField()
