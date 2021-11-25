ddd6b0fc31259d4c86f8aeeba0f53ab78e95fc34
Nicholas Ives <nickives@gmail.com>
2021-11-25 20:30:26 +0000

    Doc comment update

--- frontend/src/components/PageLayout.tsx
+++ frontend/src/components/PageLayout.tsx
@@ -10,11 +10,12 @@ export interface PageLayoutProps {
 }
 
 /**
- * This higher-order component takes an element and returns the element with
+ * This component takes an element and returns the element with
  * a Header above and a Footer below.
  *
- * @param {JSX.Element}              element Element to be wrapped
- * @param {WithHeaderAndFooterProps} props Header and Footer props
+ * @param {HeaderProps} headerProps Header props
+ * @param {FooterProps} footerProps Footer props
+ * @param {JSX.Element?} children    Element to wrap
  * @returns {JSX.Element} Wrapped element with header and footer
  */
 const PageLayout = ({
