e71c0644f7767e4bd3000392c1688cf123152fe8
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-26 12:46:17 +0000

    removed unnecessary functions

--- backend/src/api/gql/mutation/__init__.py
+++ backend/src/api/gql/mutation/__init__.py
@@ -1,13 +1,11 @@
 from .mutation_type import mutation
 
 from .create_pathway import mutation
-from .create_patient_pathway_instance import mutation
 from .create_patient import mutation
 from .create_user import mutation
 from .create_decision_point import mutation
 from .add_user_to_group import mutation
 from .create_test_result import mutation
 from .login import mutation
-
 
 type_list=[mutation]
