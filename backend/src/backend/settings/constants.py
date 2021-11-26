f1e56def1fc45dd9bc09ed0d05ad6b04b3854ffc
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-26 10:45:31 +0000

    organising settings

---
+++ backend/src/backend/settings/constants.py
@@ -0,0 +1,6 @@
+SPIRITUM_DUO={
+    "HOSPITAL_NUMBER_NAME": "MRN",
+    "HOSPITAL_NUMBER_REGEX": "^MRN[0-9]{7}$", # MRNxxxxxxx
+    "NATIONAL_NUMBER_NAME": "NHS",
+    "NATIONAL_NUMBER_REGEX": "^[0-9]{10}$" #NHSxxxxxxxxxx (for demo's sake)
+} 
