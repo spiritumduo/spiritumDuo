f41910dd1903271de4d2236122790050b2111c71
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-11-26 14:58:12 +0000

    changed input parameters, error handling

--- backend/src/api/datacreation/test_results.py
+++ backend/src/api/datacreation/test_results.py
@@ -1,29 +1,49 @@
 from api.common import db_sync_to_async
-from api.models import TestResult, TestResultMedia
+from api.models import TestResult, TestResultMedia, Patient, PatientPathwayInstance, Pathway
 from typing import List
 from api.models import Patient, PatientPathwayInstance
 
+class ReferencedItemDoesNotExistError(Exception):
+    """
+        This occurs when a referenced item (patient, user, pathway)
+        does not exist and cannot be found when it should
+    """
+
 @db_sync_to_async
 def CreateTestResult(
-    patient:Patient=None,
-    patient_pathway_instance:PatientPathwayInstance=None,
+    patient:int=None,
+    pathway:int=None,
     description:str=None,
     media_urls:List[str]=None
 ):
+    patientObject=None
     try:
-        testResult=TestResult(
-            patient=patient,
-            patient_pathway_instance=patient_pathway_instance,
-            description=description,
+        patientObject=Patient.objects.get(id=patient)
+    except Patient.DoesNotExist:
+        raise ReferencedItemDoesNotExistError("referenced item does not exist: patient (id:"+str(patient)+")")
+
+    pathwayObject=None
+    try:
+        pathwayObject=Pathway.objects.get(id=pathway)
+    except Pathway.DoesNotExist:
+        raise ReferencedItemDoesNotExistError("referenced item does not exist: pathway (id:"+str(pathway)+")")
+
+    patientPathwayInstanceObject=None
+    try:
+        patientPathwayInstanceObject=PatientPathwayInstance.objects.get(patient=patientObject, pathway=pathwayObject, is_discharged=False)
+    except PatientPathwayInstance.DoesNotExist:
+        raise ReferencedItemDoesNotExistError("referenced item does not exist: patient_pathway_instance (patient: "+str(patient)+"; pathway:"+str(pathway)+")")
+
+    testResult=TestResult(
+        patient=patientObject,
+        patient_pathway_instance=patientPathwayInstanceObject,
+        description=description,
+    )
+    testResult.save()
+    for resource_path in media_urls:
+        resource=TestResultMedia(
+            test_result=testResult,
+            resource_path=resource_path
         )
-        testResult.save()
-        for resource_path in media_urls:
-            resource=TestResultMedia(
-                test_result=testResult,
-                resource_path=resource_path
-            )
-            resource.save()
-        return testResult
-    except Exception as e:
-        print(e)
-        return False
+        resource.save()
+    return testResult
