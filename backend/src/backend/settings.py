a6be09abda4a829a50eb5667771aca03325ac26a
Nick Ives <nickives@gmail.com>
2021-11-29 14:57:56 +0000

    Reverted settings split

--- backend/src/backend/settings.py
+++ backend/src/backend/settings.py
@@ -146,3 +146,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SPIRITUM_DUO={
+    "HOSPITAL_NUMBER_NAME": "MRN",
+    "HOSPITAL_NUMBER_REGEX": "^MRN[0-9]{7}$", # MRNxxxxxxx
+    "NATIONAL_NUMBER_NAME": "NHS",
+    "NATIONAL_NUMBER_REGEX": "^[0-9]{10}$" #NHSxxxxxxxxxx (for demo's sake)
+}
