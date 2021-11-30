d06302d9c01530e3d8cf133697fc5a9e3b99ba6c
Nick Ives <nickives@gmail.com>
2021-11-30 22:33:04 +0000

    Dependancy updates Updated connection params

--- backend/src/api.py
+++ backend/src/api.py
@@ -6,14 +6,15 @@ from gino_starlette import Gino
 from models import *
 
 PG_URL = "postgresql://{user}:{password}@{host}:{port}/{database}".format(
-    host=os.getenv("DATABASE_HOSTNAME", "localhost"),
+    host=os.getenv("DATABASE_HOSTNAME", "sd-postgres"),
     port=os.getenv("DATABASE_PORT", 5432),
     user=os.getenv("DATABASE_USERNAME", "postgres"),
-    password=os.getenv("DATABASE_PASSWORD", ""),
-    database=os.getenv("DATABASE_NAME", "postgres"),
+    password=os.getenv("DATABASE_PASSWORD", "postgres"),
+    database=os.getenv("DATABASE_NAME", "starlette"),
 )
 
 app=Starlette(debug=True)
 app.mount("/graphql", GraphQL(schema, debug=True))
 
 db=Gino(app, dsn=PG_URL)
+
