e259870df0d0b22e42a5d9d0e85d132a4e3893b3
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-12-01 17:53:13 +0000

    Fixes, fixed hospital number loader

--- backend/src/dataloaders/patient.py
+++ backend/src/dataloaders/patient.py
@@ -16,6 +16,8 @@ class PatientByIdLoader(DataLoader):
             query=Patient.query.where(Patient.id.in_(keys))
             result=await conn.all(query)
         returnData={}
+        for key in keys:
+            returnData[key]=None
         for row in result:
             returnData[row.id]=row
             
@@ -36,9 +38,9 @@ class PatientByIdLoader(DataLoader):
             context[cls.loader_name] = cls(db=context['db'])
         patient=await context[cls.loader_name].load(ids)
 
-        # if not PatientByHospitalNumberLoader.loader_name in context:
-        #     context[PatientByHospitalNumberLoader.loader_name]=PatientByHospitalNumberLoader()
-        # context[PatientByHospitalNumberLoader.loader_name].prime(patient.hospital_number, patient)
+        if not PatientByHospitalNumberLoader.loader_name in context:
+            context[PatientByHospitalNumberLoader.loader_name]=PatientByHospitalNumberLoader(db=context['db'])
+        context[PatientByHospitalNumberLoader.loader_name].prime(patient.hospital_number, patient)
 
         return patient
 
@@ -51,35 +53,46 @@ class PatientByIdLoader(DataLoader):
 
 class PatientByHospitalNumberLoader(DataLoader):
     loader_name="_patient_by_hospital_number_loader"
+    _db=None
 
-    # async def fetch(self, keys)->List[Patient]:
-    #     result=None
-    #     async with self._db.acquire(reuse=False) as conn:
-    #         query=Patient.query.where(Patient.hospita.in_(keys))
-    #         result=await conn.all(query)
-    #     returnData={}
-    #     for row in result:
-    #         returnData[row.id]=row
+    def __init__(self, db):
+        super().__init__()
+        self._db=db
+
+    async def fetch(self, keys)->List[Patient]:
+        result=None
+        async with self._db.acquire(reuse=False) as conn:
+            query=Patient.query.where(Patient.hospital_number.in_(keys))
+            result=await conn.all(query)
+        returnData={}
+        for row in result:
+            returnData[row.hospital_number]=row
             
-    #     return returnData
+        return returnData
 
-    # @classmethod
-    # async def load_from_id(cls, context=None, ids=None):
-    #     if not ids:
-    #         return None
-    #     if cls.loader_name not in context:
-    #         context[cls.loader_name]=cls()
-    #     patient=await context[cls.loader_name].load(ids)
+    async def batch_load_fn(self, keys)->List[Patient]:
+        patientDict=await self.fetch(keys)
+        sortedPatients=[]
+        for key in keys:
+            sortedPatients.append(patientDict.get(key))
+        return sortedPatients
 
-    #     if not PatientByIdLoader.loader_name in context:
-    #         context[PatientByIdLoader.loader_name]=PatientByIdLoader()
-    #     context[PatientByIdLoader.loader_name].prime(patient.id, patient)
+    @classmethod
+    async def load_from_id(cls, context=None, ids=None):
+        if not ids:
+            return None
+        if cls.loader_name not in context:
+            context[cls.loader_name] = cls(db=context['db'])
+        patient=await context[cls.loader_name].load(ids)
 
-    #     return patient
+        if not PatientByIdLoader.loader_name in context:
+            context[PatientByIdLoader.loader_name]=PatientByIdLoader(db=context['db'])
+        context[PatientByIdLoader.loader_name].prime(patient.id, patient)
 
-    # async def batch_load_fn(self, keys)->List[Patient]:
-    #     patientDict=await self.fetch(keys)
-    #     sortedPatients=[]
-    #     for key in keys:
-    #         sortedPatients.append(patientDict.get(str(key)))
-    #     return sortedPatients
+        return patient
+
+    @classmethod
+    async def load_many_from_id(cls, context=None, ids=None):
+        if cls.loader_name not in context:
+            context[cls.loader_name] = cls(db=context['db'])
+        return await context[cls.loader_name].load_many(ids)
