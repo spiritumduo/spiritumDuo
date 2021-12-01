ef4f3dbff79c774974333054a7befc2a94e0469c
Nick Ives <nickives@gmail.com>
2021-12-01 21:34:25 +0000

    Moved OnPathway delete to account for FK constraints

--- backend/src/randomdata.py
+++ backend/src/randomdata.py
@@ -9,9 +9,9 @@ from SdTypes import DecisionTypes
 
 
 async def generate_random(request):
+    await OnPathway.delete.where(OnPathway.id >= 0).gino.status()
     await Pathway.delete.where(Pathway.id >= 0).gino.status()
     await Patient.delete.where(Patient.id >= 0).gino.status()
-    await OnPathway.delete.where(OnPathway.id >= 0).gino.status()
 
     await Pathway.create(name="Lung Cancer")
     await Pathway.create(name="Bronchastasis")
@@ -28,16 +28,22 @@ async def generate_random(request):
             hospital_number="MRN1234567-" + str_i,
             national_number="NHS1234567-" + str_i,
             date_of_birth=dob,
-            communication_method=DecisionTypes.TRIAGE.value
+            communication_method="LETTER"
         )
     patients = await Patient.query.gino.all()
     pathways = await Pathway.query.gino.all()
 
+    d_types = [
+        DecisionTypes.TRIAGE.value,
+        DecisionTypes.CLINIC.value
+    ]
     for p in patients:
         path = pathways[randint(0, 1)]
+        d_t = d_types[randint(0,1)]
         await OnPathway.create(
             patient=p.id,
-            pathway=path.id
+            pathway=path.id,
+            awaiting_decision_type=d_t
         )
 
 
