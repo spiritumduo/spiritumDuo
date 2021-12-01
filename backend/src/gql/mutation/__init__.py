a9ebf2ecd45957add3a0bd57cf2f351354dcea94
Nick Ives <nickives@gmail.com>
2021-12-01 17:48:22 +0000

    Login mutation

---
+++ backend/src/gql/mutation/__init__.py
@@ -0,0 +1,4 @@
+from .mutation_type import mutation
+from .login import mutation
+
+type_list = [mutation]
--- backend/src/gql/mutation/login.py
+++ backend/src/gql/mutation/login.py
@@ -1,9 +1,10 @@
-from mutation_type import mutation
-from models import User
+from .mutation_type import mutation
+from models.User import User
+
 
 @mutation.field("login")
 async def resolve_login(_=None, info=None, username=None, password=None):
-    user = User.get(1)
+    user = await User.get(1)
     if user is None:
         return None
 
