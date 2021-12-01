ecd8c76d10c096541cd32b7b100564dfac3e74d9
Nick Ives <nickives@gmail.com>
2021-12-01 13:25:06 +0000

    Moved DecisionType enum to SdTypes.py DecisionPoint & User models

--- backend/src/models/migrations.py
+++ backend/src/models/migrations.py
@@ -2,5 +2,7 @@ from . import db
 from .Patient import Patient
 from .Pathway import Pathway
 from .OnPathway import OnPathway
+from .User import User
+from .DecisionPoint import DecisionPoint
 
 db = db
