c320153ed1d48368a629cd0b90d5ecf0e1904edc
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-12-02 15:09:55 +0000

    migrated to dataloader

--- backend/src/gql/types/patient.py
+++ backend/src/gql/types/patient.py
@@ -1,28 +1,11 @@
 from ariadne.objects import ObjectType
-from models import OnPathway, DecisionPoint
-
+from dataloaders import DecisionPointsByPatient, OnPathwaysByPatient
 PatientObjectType=ObjectType("Patient")
 
 @PatientObjectType.field("pathways")    
-async def resolve_patient_pathways(obj=None, info=None, *_):
-    _gino=info.context['db']
-
-    onPathwayInstances=None
-    async with _gino.acquire(reuse=False) as conn:
-        onPathwayInstances=await conn.all(OnPathway.query.where(OnPathway.patient==obj.id))
-
-    for row in onPathwayInstances:
-        row.awaiting_decision_type=row.awaiting_decision_type.value
-    return onPathwayInstances
+async def resolve_patient_pathways(obj=None, info=None, pathwayId=None, *_):
+    return await OnPathwaysByPatient.load_from_id(context=info.context, id=obj.id)
 
 @PatientObjectType.field("decisionPoints")
-async def resolve_patient_decision_points(obj=None, info=None, *_):
-    _gino=info.context['db']
-
-    decisionPoints=None
-    async with _gino.acquire(reuse=False) as conn:
-        decisionPoints=await conn.all(DecisionPoint.query.where(DecisionPoint.patient==obj.id))
-
-    for row in decisionPoints:
-        row.decision_type=row.decision_type.value
-    return decisionPoints
+async def resolve_patient_decision_points(obj=None, info=None, pathwayId=None, decisionType=None, limit=None, *_):
+    return await DecisionPointsByPatient.load_from_id(context=info.context, id=obj.id, pathwayId=pathwayId, decisionType=decisionType, limit=limit)
