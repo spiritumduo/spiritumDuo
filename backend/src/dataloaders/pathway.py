9205770bdcba4c2bd68a5dad9bdd1c779e75cbe7
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-12-02 15:08:07 +0000

    database integration, fixes

--- backend/src/dataloaders/pathway.py
+++ backend/src/dataloaders/pathway.py
@@ -4,15 +4,27 @@ from typing import List
 
 class PathwayLoader(DataLoader):
     loader_name = "_pathway_loader"
+    _db=None
+
+    def __init__(self, db):
+        super().__init__()
+        self._db=db
 
     async def fetch(self, keys)->List[Pathway]:
-        _dict={}
+        result=None
+        async with self._db.acquire(reuse=False) as conn:
+            query=Pathway.query.where(Pathway.id.in_(keys))
+            result=await conn.all(query)
+        returnData={}
         for key in keys:
-            _dict[int(key)]=await Pathway.query.where(Pathway.id==int(key)).gino.first()
-        return _dict
+            returnData[key]=None
+        for row in result:
+            returnData[row.id]=row
+            
+        return returnData
 
     async def batch_load_fn(self, keys)->List[Pathway]:
-        pathwayDict=await self.fetch(keys)
+        pathwayDict=await self.fetch([int(i) for i in keys])
         sortedPathways=[]
         for key in keys:
             sortedPathways.append(pathwayDict.get(int(key)))
@@ -22,16 +34,15 @@ class PathwayLoader(DataLoader):
     async def load_from_id(cls, context=None, id=None):
         if not id:
             return None
-        loader_name="_pathway_loader"
-        if loader_name not in context:
-            context[loader_name]=cls()
-        pathway=await context[loader_name].load(id)
+        if cls.loader_name not in context:
+            context[cls.loader_name]=cls(db=context['db'])
+        pathway=await context[cls.loader_name].load(id)
         return pathway
 
     @classmethod
     async def load_many_from_id(cls, context=None, ids=None):
         if cls.loader_name not in context:
-            context[cls.loader_name] = cls()
+            context[cls.loader_name] = cls(db=context['db'])
         return await context[cls.loader_name].load_many(ids)
 
     @classmethod
--- backend/src/dataloaders/user.py
+++ backend/src/dataloaders/user.py
@@ -4,15 +4,27 @@ from typing import List
 
 class UserByIdLoader(DataLoader):
     loader_name = "_user_loader"
+    _db=None
+
+    def __init__(self, db):
+        super().__init__()
+        self._db=db
 
     async def fetch(self, keys)->List[User]:
-        _dict={}
+        result=None
+        async with self._db.acquire(reuse=False) as conn:
+            query=User.query.where(User.id.in_(keys))
+            result=await conn.all(query)
+        returnData={}
         for key in keys:
-            _dict[int(key)]=await User.query.where(User.id==int(key)).gino.first()
-        return _dict
+            returnData[key]=None
+        for row in result:
+            returnData[row.id]=row
+            
+        return returnData
 
     async def batch_load_fn(self, keys)->List[User]:
-        pathwayDict=await self.fetch(keys)
+        pathwayDict=await self.fetch([int(i) for i in keys])
         sortedPathways=[]
         for key in keys:
             sortedPathways.append(pathwayDict.get(int(key)))
@@ -23,12 +35,12 @@ class UserByIdLoader(DataLoader):
         if not id:
             return None
         if cls.loader_name not in context:
-            context[cls.loader_name]=cls()
+            context[cls.loader_name] = cls(db=context['db'])
         pathway=await context[cls.loader_name].load(id)
         return pathway
 
     @classmethod
     async def load_many_from_id(cls, context=None, ids=None):
         if cls.loader_name not in context:
-            context[cls.loader_name] = cls()
+            context[cls.loader_name] = cls(db=context['db'])
         return await context[cls.loader_name].load_many(ids)
