22c7417275a2f8a50df378e8fb4b75ae07e05d99
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-12-03 15:59:13 +0000

    set pathway name unique

--- backend/src/models/Pathway.py
+++ backend/src/models/Pathway.py
@@ -4,4 +4,4 @@ class Pathway(db.Model):
     __tablename__ = "pathway"
 
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String())
+    name = db.Column(db.String(), unique=True)
