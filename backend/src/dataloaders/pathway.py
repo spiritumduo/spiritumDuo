ae56ad649dcacd604adc1a6253d4704660588feb
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-12-03 16:02:36 +0000

    added pathway by name loader

--- backend/src/dataloaders/pathway.py
+++ backend/src/dataloaders/pathway.py
@@ -37,6 +37,12 @@ class PathwayByIdLoader(DataLoader):
         if cls.loader_name not in context:
             context[cls.loader_name]=cls(db=context['db'])
         pathway=await context[cls.loader_name].load(id)
+
+        if pathway:
+            if not PathwayByNameLoader.loader_name in context:
+                context[PathwayByNameLoader.loader_name]=PathwayByNameLoader(db=context['db'])
+            context[PathwayByNameLoader.loader_name].prime(pathway.name, pathway)
+
         return pathway
 
     @classmethod
@@ -48,3 +54,54 @@ class PathwayByIdLoader(DataLoader):
     @classmethod
     async def load_all(cls):
         return await Pathway.query.gino.all()
+
+
+
+class PathwayByNameLoader(DataLoader):
+    loader_name = "_pathway_by_name_loader"
+    _db=None
+
+    def __init__(self, db):
+        super().__init__()
+        self._db=db
+
+    async def fetch(self, keys)->List[Pathway]:
+        result=None
+        async with self._db.acquire(reuse=False) as conn:
+            query=Pathway.query.where(Pathway.name.in_(keys))
+            result=await conn.all(query)
+        returnData={}
+        for key in keys:
+            returnData[key]=None
+        for row in result:
+            returnData[row.name]=row
+            
+        return returnData
+
+    async def batch_load_fn(self, keys)->List[Pathway]:
+        pathwayDict=await self.fetch(keys)
+        sortedPathways=[]
+        for key in keys:
+            sortedPathways.append(pathwayDict.get(key))
+        return sortedPathways
+
+    @classmethod
+    async def load_from_id(cls, context=None, id=None):
+        if not id:
+            return None
+        if cls.loader_name not in context:
+            context[cls.loader_name]=cls(db=context['db'])
+        pathway=await context[cls.loader_name].load(id)
+
+        if pathway is not None:
+            if not PathwayByIdLoader.loader_name in context:
+                context[PathwayByIdLoader.loader_name]=PathwayByIdLoader(db=context['db'])
+            context[PathwayByIdLoader.loader_name].prime(pathway.id, pathway)
+
+        return pathway
+
+    @classmethod
+    async def load_many_from_id(cls, context=None, ids=None):
+        if cls.loader_name not in context:
+            context[cls.loader_name] = cls(db=context['db'])
+        return await context[cls.loader_name].load_many(ids)
