46f49564dd155f48b1a15f7b2d3754ea35fedbcc
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-12-07 13:21:23 +0000

    get pathways on successful login

--- backend/src/rest/login.py
+++ backend/src/rest/login.py
@@ -1,7 +1,9 @@
+from fastapi.params import Path
 from .api import _FastAPI
 from pydantic import BaseModel
-from models import User
+from models import User, Pathway
 from models.db import db
+import json
 
 WRONG_USERNAME_OR_PASSWORD_PROMPT="Incorrect username and/or password"
 
@@ -22,15 +24,27 @@ async def login(input:LoginInput):
         return{
             "error":WRONG_USERNAME_OR_PASSWORD_PROMPT
         }
-    
-    if user.password==input.password:
-        return{
-            "username":user.username,
-            "firstName":user.first_name,
-            "lastName":user.last_name,
-            "department":user.department
-        }
-    else:
+        
+    if user.password!=input.password:
         return{
             "error":WRONG_USERNAME_OR_PASSWORD_PROMPT
         }
+
+
+    pathways=None
+    async with db.acquire(reuse=False) as conn:
+        pathways=await conn.all(
+            Pathway.query
+        )
+
+    preparedPathways=[]
+    for pathway in pathways:
+        preparedPathways.append(pathway.to_dict())
+
+    return{
+        "username":user.username,
+        "firstName":user.first_name,
+        "lastName":user.last_name,
+        "department":user.department,
+        "pathways":preparedPathways
+    }
