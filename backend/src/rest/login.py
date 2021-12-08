b4c56707a3276b618f9043d3a5850e26eb8eddda
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-12-08 21:39:36 +0000

    changed login func, set redirect if already logged in

--- backend/src/rest/login.py
+++ backend/src/rest/login.py
@@ -1,10 +1,10 @@
-from authentication.authentication import CredentialsError
+from starlette.responses import RedirectResponse
+from authentication.authentication import CredentialsError, SDAuthentication, SessionAlreadyExists
 from .api import _FastAPI
 from starlette.requests import Request
 from pydantic import BaseModel
-from models import User, Pathway
+from models import Pathway
 from models.db import db
-from authentication import SDAuthentication
 WRONG_USERNAME_OR_PASSWORD_PROMPT="Incorrect username and/or password"
 NO_PERMISSIONS_PROMPT="This account is not authenticated for use in this system"
 
@@ -14,14 +14,15 @@ class LoginInput(BaseModel):
 
 @_FastAPI.post("/login/")
 async def login(request: Request):
-    authenticator=SDAuthentication()
     try:
-        authorisations, user=await authenticator.authenticate(request=request)
+        authorisations, user=await SDAuthentication.login(request=request)
     except CredentialsError:
         return{
             "error": WRONG_USERNAME_OR_PASSWORD_PROMPT
         }
-
+    except SessionAlreadyExists:
+        return RedirectResponse(url="/")
+        
     if "authenticated" not in authorisations.scopes:
         return{
             "error": NO_PERMISSIONS_PROMPT
