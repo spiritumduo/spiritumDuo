12663427129175bb94a864f651d152b908b436d1
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-12-08 15:54:49 +0000

    Added prompts, moved backend to authentication module

--- backend/src/rest/login.py
+++ backend/src/rest/login.py
@@ -1,35 +1,31 @@
-from fastapi.params import Path
+from authentication.authentication import CredentialsError
 from .api import _FastAPI
+from starlette.requests import Request
 from pydantic import BaseModel
 from models import User, Pathway
 from models.db import db
-import json
-
+from authentication import SDAuthentication
 WRONG_USERNAME_OR_PASSWORD_PROMPT="Incorrect username and/or password"
+NO_PERMISSIONS_PROMPT="This account is not authenticated for use in this system"
 
 class LoginInput(BaseModel):
     username:str
     password:str
 
 @_FastAPI.post("/login/")
-async def login(input:LoginInput):
-
-    user=None
-    async with db.acquire(reuse=False) as conn:
-        user=await conn.one_or_none(
-            User.query.where(User.username==input.username)
-        )
-
-    if user is None:
+async def login(request: Request):
+    authenticator=SDAuthentication()
+    try:
+        authorisations, user=await authenticator.authenticate(request=request)
+    except CredentialsError:
         return{
-            "error":WRONG_USERNAME_OR_PASSWORD_PROMPT
-        }
-        
-    if user.password!=input.password:
-        return{
-            "error":WRONG_USERNAME_OR_PASSWORD_PROMPT
+            "error": WRONG_USERNAME_OR_PASSWORD_PROMPT
         }
 
+    if "authenticated" not in authorisations.scopes:
+        return{
+            "error": NO_PERMISSIONS_PROMPT
+        }
 
     pathways=None
     async with db.acquire(reuse=False) as conn:
@@ -43,8 +39,8 @@ async def login(input:LoginInput):
 
     return{
         "username":user.username,
-        "firstName":user.first_name,
-        "lastName":user.last_name,
+        "firstName":user.firstName,
+        "lastName":user.lastName,
         "department":user.department,
         "pathways":preparedPathways
     }
