49ab42f82bf5554f0cc6a54d8461e814a2046a8c
Nick Ives <nickives@gmail.com>
2021-12-17 14:57:02 +0000

    Persist context to localstorage

--- frontend/src/app/context.tsx
+++ frontend/src/app/context.tsx
@@ -10,13 +10,18 @@ interface AuthContextInterface {
 }
 export const AuthContext = React.createContext<AuthContextInterface>({ updateUser: () => { } });
 export const AuthProvider = ({ children }: React.ComponentPropsWithRef<any>): JSX.Element => {
-  const [user, updateUser] = useState<User | undefined>();
+  const [user, updateContextUser] = useState<User | undefined>();
 
   if (!user) {
     const localStorageUser = loggedInUserVar(); // user has refreshed browser?
     // this will trigger a single re-render
-    if (localStorageUser) updateUser(localStorageUser);
+    if (localStorageUser) updateContextUser(localStorageUser);
   }
+
+  const updateUser = (newUser?: User) => {
+    updateContextUser(newUser);
+    loggedInUserVar(newUser);
+  };
   return (
     <AuthContext.Provider value={ { user: user, updateUser: updateUser } }>
       { children }
@@ -37,18 +42,31 @@ export const PathwayContext = React.createContext<PathwayContextInterface>({
 });
 
 export const PathwayProvider = ({ children }: React.ComponentPropsWithRef<any>): JSX.Element => {
-  const [pathwayOptions, updatePathwayOptions] = useState<PathwayOption[]>([]);
-  const [currentPathwayId, updateCurrentPathwayId] = useState<number | undefined>();
+  const [pathwayOptions, updatePathwayOptionsState] = useState<PathwayOption[]>([]);
+  const [currentPathwayId, updateCurrentPathwayIdState] = useState<number | undefined>();
 
   if (pathwayOptions.length === 0) {
     const localStoragePathwayOptions = pathwayOptionsVar();
-    if (localStoragePathwayOptions.length !== 0) updatePathwayOptions(localStoragePathwayOptions);
+    if (localStoragePathwayOptions.length !== 0) {
+      updatePathwayOptionsState(localStoragePathwayOptions);
+    }
   }
 
   if (!currentPathwayId) {
     const localStorageCurrentPathwayId = currentPathwayIdVar();
-    if (localStorageCurrentPathwayId) updateCurrentPathwayId(localStorageCurrentPathwayId);
+    if (localStorageCurrentPathwayId) updateCurrentPathwayIdState(localStorageCurrentPathwayId);
   }
+
+  const updatePathwayOptions = (pathways: PathwayOption[]) => {
+    updatePathwayOptionsState(pathways);
+    pathwayOptionsVar(pathways);
+  };
+
+  const updateCurrentPathwayId = (id: number) => {
+    updateCurrentPathwayIdState(id);
+    currentPathwayIdVar(id);
+  };
+
   return (
     <PathwayContext.Provider
       value={ {
