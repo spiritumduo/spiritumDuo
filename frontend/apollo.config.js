3230e37e9fa1b10170d7a01810e9cc2ae3d8ac85
Nick Ives <nickives@gmail.com>
2021-12-22 01:42:45 +0000

    Removed trailing slash to prevent extra redirect

--- frontend/apollo.config.js
+++ frontend/apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     service: {
       name: 'Spiritum Duo',
-      url: 'http://localhost:8080/graphql/',
+      url: 'http://localhost:8080/graphql',
     },
     excludes: ['**/__generated__/**', '**/schema.graphql'],
   },
