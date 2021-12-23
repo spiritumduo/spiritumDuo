b33cfa5bbb1d42fba607a2e606d45d80c48155eb
Nick Ives <nickives@gmail.com>
2021-12-23 02:59:11 +0000

    Updated randomdata to include milestones

--- backend/src/models/Milestone.py
+++ backend/src/models/Milestone.py
@@ -9,6 +9,7 @@ class Milestone(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     patient_id = db.Column(db.Integer(), db.ForeignKey('patient.id'), nullable=False)
     milestone_type_id = db.Column(db.Integer(), db.ForeignKey('tbl_milestone_type.id'), nullable=False)
+    decision_point_id = db.Column(db.Integer(), db.ForeignKey('decision_point.id'), nullable=False)
     on_pathway_id = db.Column(db.Integer(), db.ForeignKey('on_pathway.id'), nullable=False)
     added_at = db.Column(db.DateTime(), server_default=func.now(), nullable=False)
     updated_at = db.Column(
