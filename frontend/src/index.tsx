8f0d97506acdee6a742d89aa71950c0a42614cfa
Nick Ives <nickives@gmail.com>
2022-01-04 11:49:17 +0000

    Service workers to enable PWA functionality

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,6 +7,7 @@ import './index.css';
 import App from 'app/App';
 import reportWebVitals from 'reportWebVitals';
 import scalarLink from 'app/scalars';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 // TODO: Disable this for production! This is just because we are on a different port
 const link = new HttpLink({
@@ -39,6 +40,11 @@ ReactDOM.render(
   // eslint-disable-next-line no-undef
   document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
