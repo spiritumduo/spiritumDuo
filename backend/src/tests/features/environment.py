70758dc4f589502d9eb5dfef6165234530ef7da9
Nick Ives <nickives@gmail.com>
2022-01-07 16:31:44 +0000

    Make our own event loops to play with Behave

--- backend/src/tests/features/environment.py
+++ backend/src/tests/features/environment.py
@@ -1,43 +1,87 @@
+import asyncio
+from random import randint
+
+from bcrypt import hashpw, gensalt
+from behave.api.async_step import async_run_until_complete
 from starlette.config import environ
 environ['TESTING'] = "True"
 
 from models.db import db, TEST_DATABASE_URL
+from models import User
 from api import app
 from behave import fixture, use_fixture
 from starlette.testclient import TestClient
 from sqlalchemy_utils import database_exists, create_database, drop_database
-from sqlalchemy import create_engine, MetaData
 
 
 @fixture
-def test_client(context):
+def create_test_client(context):
     with TestClient(app) as client:
         context.client = client
         yield context.client
 
 
 @fixture
-async def db_transaction(context):
-    print('tx')
-    async with db.transaction() as tx:
-        context.tx = tx
-        yield context.tx
-        tx.raise_rollback()
+def db_start_transaction(context):
+    loop = asyncio.get_event_loop()
+    conn = loop.run_until_complete(context.engine.acquire())
+    tx = loop.run_until_complete(conn.transaction())
+    context.conn = conn
+    yield
+    loop.run_until_complete(tx.rollback())
+
 
 @fixture
 def create_test_database(context):
     url = TEST_DATABASE_URL
-    engine = create_engine(url)
     assert not database_exists(url)
     create_database(url)
-    metadata = MetaData()
-    metadata.create_all(engine)
+    loop = asyncio.get_event_loop()
+    context.engine = loop.run_until_complete(db.set_bind(TEST_DATABASE_URL))
+    loop.run_until_complete(db.gino.create_all())
     yield
     drop_database(url)
 
 
+CLINICIAN = {
+    "firstName": "MIKE",
+    "lastName": "SMITH",
+    "username": f"MIKE.SMITH{randint(1000,9999)}",
+    "password": "VERYSECUREPASSWORD",
+    "department": "CIH",
+}
+
+
+@fixture
+def add_test_user(context):
+    print('test user')
+    loop = asyncio.get_event_loop()
+    context.user = loop.run_until_complete(User.create(
+        username=CLINICIAN['username'],
+        password=hashpw(CLINICIAN["password"].encode('utf-8'), gensalt()).decode('utf-8'),
+        first_name=CLINICIAN['firstName'],
+        last_name=CLINICIAN['lastName'],
+        department=CLINICIAN['department']
+    ))
+
+
+@fixture
+def login_test_user(context):
+    print('login test')
+    res = context.client.post(
+        url='/rest/login',
+        json={
+            "username": CLINICIAN['username'],
+            "password": CLINICIAN['password']
+        }
+    )
+    context.user.cookies = res.cookies
+
+
 def before_feature(context, _):
     use_fixture(create_test_database, context)
-    use_fixture(db_transaction, context)
-    use_fixture(test_client, context)
+    use_fixture(db_start_transaction, context)
+    use_fixture(add_test_user, context)
+    use_fixture(create_test_client, context)
+    use_fixture(login_test_user, context)
 
