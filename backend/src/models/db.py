19e7e6d623322b8604ce23d1151113ce57e4c3a2
Nick Ives <nickives@gmail.com>
2022-01-07 17:47:37 +0000

    Updated to support test db

--- backend/src/models/db.py
+++ backend/src/models/db.py
@@ -1,14 +1,27 @@
 import os
 from gino_starlette import Gino
 
-PG_URL = "postgresql://{user}:{password}@{host}:{port}/{database}".format(
-    host=os.getenv("DATABASE_HOSTNAME", "sd-postgres"),
-    port=os.getenv("DATABASE_PORT", 5432),
-    user=os.getenv("DATABASE_USERNAME", "postgres"),
-    password=os.getenv("DATABASE_PASSWORD", "postgres"),
-    database=os.getenv("DATABASE_NAME", "starlette"),
+DB_STR = "postgresql://{user}:{password}@{host}:{port}/{database}"
+
+TESTING = os.getenv('TESTING', default=False)
+DATABASE_URL = DB_STR.format(
+    host=os.getenv("DATABASE_HOSTNAME"),
+    port=os.getenv("DATABASE_PORT"),
+    user=os.getenv("DATABASE_USERNAME"),
+    password=os.getenv("DATABASE_PASSWORD"),
+    database=os.getenv("DATABASE_NAME"),
 )
 
-db=Gino(
-    dsn=PG_URL
+# The database has 'test_' prepended here
+TEST_DATABASE_URL = DB_STR.format(
+    host=os.getenv("DATABASE_HOSTNAME"),
+    port=os.getenv("DATABASE_PORT"),
+    user=os.getenv("DATABASE_USERNAME"),
+    password=os.getenv("DATABASE_PASSWORD"),
+    database="test_" + os.getenv("DATABASE_NAME"),
 )
+
+if TESTING:
+    db = Gino(dsn=TEST_DATABASE_URL)
+else:
+    db = Gino(dsn=DATABASE_URL)
