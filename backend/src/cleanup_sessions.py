266ae38f2a5072b9ff363944b272586025a5eae6
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2022-01-13 17:37:17 +0000

    added cleanup script

---
+++ backend/src/cleanup_sessions.py
@@ -0,0 +1,13 @@
+from models.db import db, DATABASE_URL
+from models import Session
+from config import config
+from asyncio import get_event_loop
+from datetime import datetime, timedelta
+session_expiry=int(config['SESSION_EXPIRY_LENGTH'])
+
+async def cleanup():
+    await db.set_bind(DATABASE_URL)
+    await Session.delete.where(Session.expiry<datetime.now()+timedelta(seconds=session_expiry)).gino.status()
+
+loop=get_event_loop()
+loop.run_until_complete(cleanup())
