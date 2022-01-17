4bf636194a6de6bf6866f6141334cec42b5e5ea2
Nick Ives <nickives@gmail.com>
2022-01-17 17:19:51 +0000

    Patient return data and creation

--- pseudotie/src/main.py
+++ pseudotie/src/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+import json
+
+from fastapi import FastAPI, Request
+from datetime import date
+from models import Patient, db
+from asyncpg.exceptions import UniqueViolationError
+from pydantic import BaseModel
 from starlette.responses import JSONResponse
 
 app = FastAPI()
@@ -14,6 +20,37 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
+class PatientInput(BaseModel):
+    hospital_number: str
+    national_number: str
+    communication_method: str
+    first_name: str
+    last_name: str
+    date_of_birth: date
+
+
+@app.post("/patient/")
+async def patient_post(_: Request, input: PatientInput):
+    """
+    Create patient
+    :param _: Request - ignored
+    :param input: PatientInput - patient data to input
+    :return: JSONResponse of created patient
+    """
+    try:
+        patient = await Patient.create(
+            hospital_number=input.hospital_number,
+            national_number=input.national_number,
+            communication_method=input.communication_method,
+            first_name=input.first_name,
+            last_name=input.last_name,
+            date_of_birth=input.date_of_birth,
+        )
+        return patient
+    except UniqueViolationError:
+        return JSONResponse(status_code=409)
+
+
 @app.get("/patient/hospital/{id}")
 async def patient_hospital_id(id: str):
     """
@@ -21,7 +58,8 @@ async def patient_hospital_id(id: str):
     :param id: String ID
     :returns JSONResponse containing Patient or null
     """
-    pass
+    patient = await Patient.query.where(Patient.hospital_number == str(id)).gino.first()
+    return patient
 
 
 @app.get("/patient/national/{id}")
@@ -31,7 +69,8 @@ async def patient_national_id(id: str):
     :param id: String ID
     :returns JSONResponse containing
     """
-    pass
+    patient = await Patient.query.where(Patient.national_number == str(id)).gino.first()
+    return patient
 
 
 @app.post("/milestone")
@@ -51,5 +90,8 @@ async def milestone_id(id: str):
     :return: JSONResponse containing Milestone requested or null
     """
     pass
+
+
+db.init_app(app)
 
 
