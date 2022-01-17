9cda671e6f431c0e1d241e795b57663dec0a1f84
Nick Ives <nickives@gmail.com>
2022-01-17 17:19:36 +0000

    Add unique constraint

--- pseudotie/src/models/Patient.py
+++ pseudotie/src/models/Patient.py
@@ -4,8 +4,8 @@ class Patient(db.Model):
     __tablename__ = "tbl_patient"
 
     id = db.Column(db.Integer(), primary_key=True)
-    hospital_number = db.Column(db.String(), nullable=False)
-    national_number = db.Column(db.String(), nullable=False)
+    hospital_number = db.Column(db.String(), nullable=False, unique=True)
+    national_number = db.Column(db.String(), nullable=False, unique=True)
     communication_method = db.Column(db.String(), nullable=False)
     first_name = db.Column(db.String(), nullable=False)
     last_name = db.Column(db.String(), nullable=False)
