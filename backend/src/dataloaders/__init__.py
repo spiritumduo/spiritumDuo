a88a39c91dbb6fa92ab8fd5699a40af38e0b15d9
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2022-01-18 15:54:19 +0000

    added dataloaders

--- backend/src/dataloaders/__init__.py
+++ backend/src/dataloaders/__init__.py
@@ -1,7 +1,7 @@
-from .patient import PatientByIdLoader, PatientByHospitalNumberLoader
+from .patient import PatientByIdLoader, PatientByHospitalNumberLoader, PatientByHospitalNumberFromIELoader
 from .pathway import PathwayByIdLoader, PathwayByNameLoader
 from .user import UserByIdLoader, UserByUsernameLoader
 from .decision_point import DecisionPointLoader, DecisionPointsByPatient, DecisionPointsByOnPathway
 from .on_pathway import OnPathwayByIdLoader, OnPathwaysByPatient
-from .milestone import MilestoneByDecisionPointLoader
+from .milestone import MilestoneByDecisionPointLoader, MilestoneByReferenceIdFromIELoader
 from .milestone_type import MilestoneTypeLoader
