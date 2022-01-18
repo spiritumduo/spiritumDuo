c32e535e7bc7efed8e30ba6a86884beab6a195d8
Nick Ives <nickives@gmail.com>
2022-01-18 11:59:34 +0000

    Integration Engine class design

--- backend/src/SDIE/integration_engine.py
+++ backend/src/SDIE/integration_engine.py
@@ -1,24 +1,87 @@
 import requests
-from typing import List
+from models import Milestone, Patient
+from abc import ABC, abstractmethod
+from typing import List, Optional
 
-class IntegrationEngine:
-    def __init__(self, authToken:str=None):
-        self._authToken=authToken
-    
-    def load_patient(self, recordId:str=None, hospitalNumber:str=None, nationalNumber:str=None):
+
+class IntegrationEngine(ABC):
+    """
+    Integration Engine Abstract Base Class
+
+    This class represents the interface SD uses to communicate with a backend hospital system.
+    """
+
+    @abstractmethod
+    async def load_patient(self, record_id: str = None) -> Optional[Patient]:
+        """
+        Load single patient
+        :param record_id: String ID of patient
+        :return: Patient if found, null if not
+        """
         pass
 
-    def load_many_patients(self, recordId:List=None, hospitalNumber:List=None, nationalNumber:List=None):
+    @abstractmethod
+    async def load_many_patients(self, record_ids: List = None) -> List[Optional[Patient]]:
+        """
+        Load many patients
+        :param record_ids: List of patient ids to load
+        :return: List of patients, or empty list if none found
+        """
         pass
 
-    def create_milestone(self, recordId:str=None):
+    @abstractmethod
+    async def create_milestone(self, milestone: Milestone = None) -> str:
+        """
+        Create a Milestone
+        :param milestone: Milestone to create
+        :return: String ID of created milestone
+        """
         pass
 
-    def load_milestone(self, recordId:str=None):
+    @abstractmethod
+    async def load_milestone(self, record_id: str = None) -> Optional[Milestone]:
+        """
+        Load a Milestone
+        :param record_id: ID of milestone to load
+        :return: Milestone, or null if milestone not found
+        """
         pass
 
-    def login(self, username:str=None, password:str=None):
+    @abstractmethod
+    async def load_many_milestones(self, record_ids: str = None) -> List[Optional[Milestone]]:
+        """
+        Load many milestones
+        :param record_ids: IDs of milestones to load
+        :return: List of milestones, or empty list if none found
+        """
         pass
-    
-    def create_user(self, username:str=None, password:str=None, first_name:str=None, last_name:str=None, department:str=None):
+
+
+class PseudoIntegrationEngine(IntegrationEngine):
+    """
+    Pseudo Integration Engine
+
+    This is the Integration Engine implementation for the pseudo-trust backend.
+    """
+
+    def __init__(self, auth_token: str = None):
+        """
+        Constructor. Requires an authentication token.
+        :param auth_token: String token to be supplied to pseudo-trust with each request
+        """
+        self._authToken = auth_token
+
+    async def load_patient(self, record_id: str = None) -> Optional[Patient]:
+        pass
+
+    async def load_many_patients(self, record_ids: List = None) -> List[Optional[Patient]]:
+        pass
+
+    async def create_milestone(self, milestone: Milestone = None) -> str:
+        pass
+
+    async def load_milestone(self, record_id: str = None) -> Optional[Milestone]:
+        pass
+
+    async def load_many_milestones(self, record_ids: str = None) -> List[Optional[Milestone]]:
         pass
