222e7c905b0edf1d0d39fcc59fbeadc24be71fe4
Joe Channing <jcfoxtrot54@protonmail.com>
2022-01-21 18:38:44 +0000

    set IDs as unique

--- backend/src/models/Patient.py
+++ backend/src/models/Patient.py
@@ -4,5 +4,5 @@ class Patient(db.Model):
     __tablename__ = "tbl_patient"
 
     id = db.Column(db.Integer(), primary_key=True)
-    hospital_number = db.Column(db.String(), nullable=False)
-    national_number = db.Column(db.String(), nullable=False)
+    hospital_number = db.Column(db.String(), nullable=False, unique=True)
+    national_number = db.Column(db.String(), nullable=False, unique=True)
