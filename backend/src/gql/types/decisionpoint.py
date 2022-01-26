b596132b08b11eb0f397e0832587e1d2856e3042
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2022-01-26 18:28:02 +0000

    added `milestoneResolutions` to gql resolvers

--- backend/src/gql/types/decisionpoint.py
+++ backend/src/gql/types/decisionpoint.py
@@ -13,14 +13,23 @@ async def resolve_on_pathway_clinician(obj=None, info=None, *_):
 async def resolve_on_pathway_pathway(obj=None, info=None, *_):
     return await OnPathwayByIdLoader.load_from_id(context=info.context, id=obj.on_pathway_id)
 
+@DecisionPointObjectType.field("milestoneResolutions")
+async def resolve_on_pathway_pathway(obj=None, info=None, *_):
+    query = Milestone.query.where(Milestone.fwd_decision_point_id == obj.id)
+    query.order_by(Milestone.id.desc())
+    async with info.context['db'].acquire(reuse=False) as conn:
+        milestones = await conn.all(query)
+        for m in milestones:
+            MilestoneByDecisionPointLoader.prime_with_context(info.context, m.id, m)
+        return milestones
 
 @DecisionPointObjectType.field("milestones")
 async def resolve_decision_point_milestones(obj=None, info=None, *_):
-    query = Milestone.query.where(Milestone.decision_point_id == obj.id)
     # TODO: added another dataloader to get by decision point ID
     # tieMilestone=await MilestoneByReferenceIdFromIELoader.load_from_id(context=info.context, id=obj.id)
     # query.order_by(tieMilestone.added_at)
-    query.order_by(Milestone.id.desc())
+    query = Milestone.query.where(Milestone.decision_point_id == obj.id)
+    query.order_by(Milestone.added_at.desc())
     async with info.context['db'].acquire(reuse=False) as conn:
         milestones = await conn.all(query)
         for m in milestones:
