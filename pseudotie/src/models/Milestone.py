06df19745247534ec9e6224b67d106511493ab7d
Joe <jcfoxtrot54@protonmail.com>
2022-02-01 13:36:42 +0000

    added milestone type ref id to pseudotie for test result generation

--- pseudotie/src/models/Milestone.py
+++ pseudotie/src/models/Milestone.py
@@ -11,5 +11,6 @@ class Milestone(db.Model):
         Enum(MilestoneState, native_enum=False), default=MilestoneState.INIT.value,
         server_default=MilestoneState.INIT.value, nullable=False, native_enum=False
     )
+    type_reference_id = db.Column(db.String(), nullable=False)
     added_at = db.Column(db.DateTime(), server_default=func.now(), nullable=False)
     updated_at = db.Column(db.DateTime(), server_default=func.now(), onupdate=func.now(), nullable=False)
