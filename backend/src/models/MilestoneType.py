49ccedc54adbc8e791513c14773daa2f49b717cb
Joe <jcfoxtrot54@protonmail.com>
2022-02-01 13:40:28 +0000

    added `ref_id` to `MilestoneType` model

--- backend/src/models/MilestoneType.py
+++ backend/src/models/MilestoneType.py
@@ -6,4 +6,4 @@ class MilestoneType(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(), nullable=False)
     ref_name = db.Column(db.String(), nullable=False)
-    # ref_ID = db.Column(db.String(), nullable=False)
+    ref_id = db.Column(db.String(), nullable=False)
