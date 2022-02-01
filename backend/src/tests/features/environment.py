3d976caf98d9f321a24eacd999a14b4f4dd138b2
Joe <jcfoxtrot54@protonmail.com>
2022-02-01 13:40:15 +0000

    added ref id to milestone type

--- backend/src/tests/features/environment.py
+++ backend/src/tests/features/environment.py
@@ -65,7 +65,8 @@ def add_test_data(context):
     ))
     context.milestone_one=loop.run_until_complete(MilestoneType.create(
         name="Test Milestone One",
-        ref_name="test_milestone_one"
+        ref_name="test_milestone_one",
+        ref_id="1234567890"
     ))
 
 @fixture
