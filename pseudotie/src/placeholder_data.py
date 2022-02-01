d9baf0596eefc407910155fb0ea45ac190ae638c
Joe <jcfoxtrot54@protonmail.com>
2022-02-01 15:49:12 +0000

    fixed loading of placeholder data

--- pseudotie/src/placeholder_data.py
+++ pseudotie/src/placeholder_data.py
@@ -1,7 +1,8 @@
 import json
 
 try:
-    _TEST_RESULT_JSON=open("milestone_demo_data.json", "r")
-    TEST_RESULT_DATA=json.loads(_TEST_RESULT_JSON)
-except:
+    _TEST_RESULT_FILE=open("milestone_demo_data.json", "r")
+    TEST_RESULT_DATA=json.load(_TEST_RESULT_FILE)
+    _TEST_RESULT_FILE.close()
+except Exception as e:
     TEST_RESULT_DATA=[]
