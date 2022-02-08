934be1b6de0da0890f97aa7b75aa576a5b03496c
Joe <joechanning@protonmail.com>
2022-02-08 18:23:09 +0000

    added `importMilestone` to data creator, gql

--- backend/src/datacreators/__init__.py
+++ backend/src/datacreators/__init__.py
@@ -2,3 +2,4 @@ from .patient import CreatePatient
 from .pathway import CreatePathway
 from .decision_point import CreateDecisionPoint
 from .user import CreateUser
+from .milestone import ImportMilestone
