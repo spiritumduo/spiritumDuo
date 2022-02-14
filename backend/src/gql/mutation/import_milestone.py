7978ba28deb5ba0efa3e78f59fd9299d13af805c
Joe <joechanning@protonmail.com>
2022-02-14 23:54:46 +0000

    completed `importMilestone` mutation

--- backend/src/gql/mutation/import_milestone.py
+++ backend/src/gql/mutation/import_milestone.py
@@ -1,20 +1,16 @@
 from .mutation_type import mutation
 from datacreators import ImportMilestone
 from authentication.authentication import needsAuthorization
+from graphql.type import GraphQLResolveInfo
 
 @mutation.field("importMilestone")
 @needsAuthorization(["authenticated"])
-    data={
-        "onPathwayId":input["onPathwayId"],
-        "milestoneTypeId":input["milestoneTypeId"],
-        "description":input["description"],
-        "currentState":input["currentState"],
-    }
-    
 async def resolver(obj=None, info:GraphQLResolveInfo=None, input:dict=None):   
-    return {
-        "milestone": await ImportMilestone(
-            context=info.context,
-            **data
-        )
-    }
+    milestone = await ImportMilestone(
+        context = info.context,
+        onPathwayId = input["onPathwayId"],
+        milestoneTypeId = input["milestoneTypeId"],
+        description = input["description"],
+        currentState = input["currentState"],
+    )
+    return milestone
