14fe2aa990491b0c8fc383d0a7c85489da65b6fa
Joe <joechanning@protonmail.com>
2022-02-15 12:30:15 +0000

    Added error handling, patched edge case

--- backend/src/datacreators/patient.py
+++ backend/src/datacreators/patient.py
@@ -4,11 +4,12 @@ from containers import SDContainer
 from models import Patient, OnPathway, Milestone, Pathway
 from datetime import date, datetime
 from gettext import gettext as _
-from dataloaders import PathwayByIdLoader
+from dataloaders import PathwayByIdLoader, OnPathwaysByPatient, MilestoneTypeLoader
 from config import config as SdConfig
 from typing import Optional, List, Union
 from trustadapter.trustadapter import Patient_IE, TestResult_IE, TrustAdapter, TestResultRequest_IE
 from common import ReferencedItemDoesNotExistError, PatientNotInIntegrationEngineError, DataCreatorInputErrors
+from asyncpg.exceptions import UniqueViolationError
 
 @inject
 async def CreatePatient(
@@ -25,13 +26,27 @@ async def CreatePatient(
     milestones:List[Milestone]={},
     trust_adapter: TrustAdapter = Provide[SDContainer.trust_adapter_service]
 ):
-    if not context:
-        raise ReferencedItemDoesNotExistError("Context is not provided.")
-    _db=context['db']
-
-    auth_token = context['request'].cookies['SDSESSION']
+    if context is None:
+        raise TypeError("Context is not provided.")
+    if pathwayId is None:
+        raise TypeError("Pathway ID not provided.")
     
+    auth_token = context['request'].cookies['SDSESSION']
     errors=DataCreatorInputErrors()
+
+
+    milestoneTypesFromMilestones=await MilestoneTypeLoader.load_many_from_id(
+        context=context,
+        ids=[int(m['milestoneTypeId']) for m in milestones]
+    )
+    if None in milestoneTypesFromMilestones:
+        raise ReferencedItemDoesNotExistError("Milestone type specified does not exist")
+
+    _pathway:Pathway=await PathwayByIdLoader.load_from_id(context=context, id=pathwayId)
+    if _pathway is None:
+        raise ReferencedItemDoesNotExistError("Pathway provided does not exist.")
+
+
     # check if hospital number provided matches regex in configuration
     if re.search(SdConfig["HOSPITAL_NUMBER_REGEX"], hospital_number) is None: 
         errors.addError(field="hospital_number", message="Input does not match expected pattern")
@@ -41,59 +56,66 @@ async def CreatePatient(
     
     if errors.hasErrors(): return errors
 
-    if not pathwayId:
-        raise ReferencedItemDoesNotExistError("Pathway ID not provided.")
-
-    _pathway:Pathway=await PathwayByIdLoader.load_from_id(context=context, id=pathwayId)
-    if _pathway is None:
-        raise ReferencedItemDoesNotExistError("Pathway provided does not exist.")
     
-    _patient:Patient_IE = await trust_adapter.load_patient(hospitalNumber=hospital_number, auth_token=auth_token)
-    if _patient:
-        if _patient.first_name!=first_name:
+    patientFromTrustAdapter:Patient_IE = await trust_adapter.load_patient(hospitalNumber=hospital_number, auth_token=auth_token)
+    if patientFromTrustAdapter:
+        """
+        The patient exists via the trust adapter
+        """
+        if patientFromTrustAdapter.first_name!=first_name:
             errors.addError(field="first_name", message="Input does not match patient from external system")
-        if _patient.last_name!=last_name:
+        if patientFromTrustAdapter.last_name!=last_name:
             errors.addError(field="last_name", message="Input does not match patient from external system")
-        if _patient.date_of_birth!=date_of_birth:
+        if patientFromTrustAdapter.date_of_birth!=date_of_birth:
             errors.addError(field="date_of_birth", message="Input does not match patient from external system")
-        if _patient.national_number!=national_number:
+        if patientFromTrustAdapter.national_number!=national_number:
             errors.addError(field="national_number", message="Input does not match patient from external system")
-        
-        if errors.hasErrors(): return errors
-
-        # if the patient does already exist
-        existingOnPathwayQuery=OnPathway.query.where(OnPathway.patient_id==_patient.id).where(OnPathway.pathway_id==_pathway.id).where(OnPathway.is_discharged==False)
-        
-        async with _db.acquire(reuse=False) as conn:
-            existingOnPathway:Union[OnPathway, None]=await conn.one_or_none(existingOnPathwayQuery)
-        
-        if existingOnPathway: # if there is an active pathway instance
-            errors.addError(field="patient", message="Patient already belongs to this pathway and is not discharged")
-
         if errors.hasErrors(): return errors
     else:
-        _patient = Patient_IE(
-            first_name=first_name,
-            last_name=last_name,
-            communication_method=communication_method,
-            hospital_number=hospital_number,
-            national_number=national_number,
-            date_of_birth=date_of_birth
+        """
+        The patient does not exist via trust adapter and needs to be created
+        """
+        patientFromTrustAdapter:Patient_IE = await trust_adapter.create_patient(
+            patient=Patient_IE(
+                first_name=first_name,
+                last_name=last_name,
+                communication_method=communication_method,
+                hospital_number=hospital_number,
+                national_number=national_number,
+                date_of_birth=date_of_birth
+            ), 
+            auth_token=auth_token
         )
-
-        _patient:Patient_IE = await trust_adapter.create_patient(patient=_patient, auth_token=auth_token)
-        if _patient is None:
-            raise PatientNotInIntegrationEngineError(hospital_number, national_number)
         
-    
-    patient:Patient = await Patient.create(
-        hospital_number=_patient.hospital_number,
-        national_number=_patient.national_number
-    )
-    _patient.id = patient.id
+    try:
+        """
+        Create the patient in backend
+        """
+        patientFromLocal:Patient = await Patient.create(
+            hospital_number=patientFromTrustAdapter.hospital_number,
+            national_number=patientFromTrustAdapter.national_number
+        )
+    except UniqueViolationError as e:
+        """
+        The patient exists in backend already
+        """
+        if national_number != patientFromTrustAdapter.national_number:
+            raise Exception("""
+                A patient does not exist via TA, but does exist locally. An attempt was made to use the local record,
+                however national_number does not match the expected value via TA
+            """)
 
+        print(f"The patient {hospital_number} already exists in backend, while not existing in pseudotie")
+
+        patientFromLocal=await Patient.query.where(Patient.hospital_number==patientFromTrustAdapter.hospital_number).gino.one_or_none()
+    
+    patientOnPathway:Union[OnPathway, None]=await OnPathwaysByPatient.load_from_id(context=context, id=patientFromLocal.id, pathwayId=_pathway.id, isDischarged=False)
+    if patientOnPathway is not None and len(patientOnPathway)>0: # if there is an active pathway instance
+        errors.addError(field="patient", message="Patient already belongs to this pathway and is not discharged")
+        return errors
+    
     onPathwayInformation={
-        'patient_id': _patient.id,
+        'patient_id': patientFromLocal.id,
         'pathway_id': _pathway.id,
         'awaiting_decision_type': awaiting_decision_type,
         'is_discharged': False,
@@ -116,4 +138,7 @@ async def CreatePatient(
             milestone_type_id=int(milestone["milestoneTypeId"]),
             test_result_reference_id=str(test_result.id)
         )
-    return _patient
+
+    returnPatient=patientFromTrustAdapter
+    returnPatient.id=patientFromLocal.id
+    return returnPatient
