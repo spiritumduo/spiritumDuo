cf058ba3a96862ba96906baae9c19bd99e3ac2b3
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2022-02-18 12:08:19 +0000

    added `is_test_request` to milestone types

--- backend/src/models/MilestoneType.py
+++ backend/src/models/MilestoneType.py
@@ -8,3 +8,4 @@ class MilestoneType(db.Model):
     ref_name = db.Column(db.String(), nullable=False)
     is_discharge = db.Column(db.Boolean(), nullable=False, default=False)
     is_checkbox_hidden = db.Column(db.Boolean(), nullable=False, default=False, server_default="FALSE")
+    is_test_request = db.Column(db.Boolean(), nullable=False, default=False)
