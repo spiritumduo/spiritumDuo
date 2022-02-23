ce4d4d2577ed883929e90641e08f374003ecd30f
Nick Ives <nickives@gmail.com>
2022-02-23 15:16:20 +0000

    Added test coverage

--- frontend/src/components/PathwayComplete.spec.tsx
+++ frontend/src/components/PathwayComplete.spec.tsx
@@ -3,3 +3,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { composeStories } from '@storybook/testing-react';
 import * as stories from './PathwayComplete.stories';
+
+const { Default } = composeStories(stories);
+
+it('Should display pathway completed', () => {
+  render(<Default />);
+  expect(screen.getByText(/The pathway is now complete!/i)).toBeInTheDocument();
+});
