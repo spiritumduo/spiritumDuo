e8fc167d14b64cca7058e754409396177a18bec8
Joe <joechanning@protonmail.com>
2022-03-03 20:21:20 +0000

    Added input component

---
+++ frontend/src/components/nhs_style/input/Input.stories.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { MemoryRouter } from 'react-router';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Input from './Input';
+
+export default {
+  title: 'Input',
+  component: Input,
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input { ...args } />;
+
+export const Default = Template.bind({});
+Default.args = { type: 'text' };
