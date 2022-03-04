25becc363f05df5660e5c44bf61fed929384c4d8
Joe <joechanning@protonmail.com>
2022-03-04 17:09:46 +0000

    Replaced with `SdHeader` component

--- frontend/src/components/PageLayout.tsx
+++ frontend/src/components/PageLayout.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useContext } from 'react';
-// import Header from 'components/Header';
-import { Footer, Header, Select } from 'nhsuk-react-components';
 import Notification from 'components/Notification';
 import { AuthContext, PathwayContext } from 'app/context';
 import './pagelayout.css';
-import sdInvertedImage from '../static/i/sd_inverted.png';
+import SdHeader from './SdHeader';
+import SdFooter from './SdFooter';
 
 export interface PageLayoutProps {
     children?: JSX.Element
@@ -23,37 +22,15 @@ const PageLayout = ({
   const actualCurrentPathwayId = currentPathwayId || pathwayOptions[0].id;
   return (
     <div>
-      {/* <Header
+      <SdHeader
         pathwayOptions={ pathwayOptions }
         currentPathwayId={ actualCurrentPathwayId }
         pathwayOnItemSelect={ () => console.log('item selected') }
         searchOnSubmit={ () => console.log('search submit') }
-        usersName={ `${user?.firstName} ${user?.lastName}` }
-      /> */}
-      <Header>
-        <Header.Container>
-          <a href="/app"><img src={ sdInvertedImage } alt="Spiritum Duo logo" className="nhsuk-header__logo" height="40px" /></a>
-          <Header.Content>
-            <Header.MenuToggle />
-            <Header.Search />
-          </Header.Content>
-        </Header.Container>
-        <Header.Nav>
-          <Header.NavItem href="/" mobileOnly>
-            Home
-          </Header.NavItem>
-          <Header.NavItem style={ { fontSize: '1.1875rem' } } href="">Home</Header.NavItem>
-          <Header.NavItem style={ { fontSize: '1.1875rem' } } href="mdt">MDT</Header.NavItem>
-          <Header.NavItem style={ { fontSize: '1.1875rem' } } href="add-patient" disabled>Add Patient</Header.NavItem>
-          <Header.NavItem style={ { fontSize: '1.1875rem' } } href="logout">Logout ({`${user?.firstName} ${user?.lastName}`})</Header.NavItem>
-        </Header.Nav>
-      </Header>
+        user={ user }
+      />
       {children}
-      <Footer>
-        <Footer.List>
-          <Footer.ListItem href="/">Landing page</Footer.ListItem>
-        </Footer.List>
-      </Footer>
+      <SdFooter />
       <Notification />
     </div>
   );
