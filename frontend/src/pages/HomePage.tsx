734735bfbbbc6e2321861d9a4eb42a315ddad201
Joe <joechanning@protonmail.com>
2022-03-04 01:33:56 +0000

    Added tabs

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -4,6 +4,8 @@ import { PathwayContext } from 'app/context';
 import WrappedPatientList from 'components/WrappedPatientList';
 import { Link } from 'react-router-dom';
 import { getPatientOnPathwayConnection_getPatientOnPathwayConnection_edges_node } from 'components/__generated__/getPatientOnPathwayConnection';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { Container } from 'nhsuk-react-components';
 
 export interface HomePageProps {
   patientsPerPage: number;
@@ -21,20 +23,35 @@ const HomePage = ({ patientsPerPage }: HomePageProps): JSX.Element => {
   ) => <Link style={ { padding: '0' } } to={ `/decision/triage/${patient.hospitalNumber}` }>{ `${patient.firstName} ${patient.lastName}` }</Link>;
 
   return (
-    <div className="container">
-      <div className="col-12 col-xxl-6 row mt-1 justify-content-start">
-        <div>
-          <h3>Outstanding Decisions</h3>
-          <WrappedPatientList
-            pathwayId={ pathwayId.toString() }
-            patientsToDisplay={ patientsPerPage }
-            linkFactory={ linkFactory }
-            outstanding
-            underCareOf
-            includeDischarged={ false }
-          />
+    <div className="row justify-content-center">
+      <Tabs>
+        <TabList>
+          <Tab>Outstanding patients</Tab>
+          <Tab>All patients</Tab>
+        </TabList>
+        <div className="col-10 offset-1">
+          <TabPanel>
+            <WrappedPatientList
+              pathwayId={ pathwayId.toString() }
+              patientsToDisplay={ patientsPerPage }
+              linkFactory={ linkFactory }
+              outstanding
+              underCareOf
+              includeDischarged={ false }
+            />
+          </TabPanel>
+          <TabPanel>
+            <WrappedPatientList
+              pathwayId={ pathwayId.toString() }
+              patientsToDisplay={ patientsPerPage }
+              linkFactory={ linkFactory }
+              outstanding
+              underCareOf
+              includeDischarged={ false }
+            />
+          </TabPanel>
         </div>
-      </div>
+      </Tabs>
     </div>
   );
 };
