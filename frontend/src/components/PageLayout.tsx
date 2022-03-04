6aa5fb5e3a02da21d829ec5e72447cf03843bd5a
Joe <joechanning@protonmail.com>
2022-03-04 01:33:13 +0000

    changed header style

--- frontend/src/components/PageLayout.tsx
+++ frontend/src/components/PageLayout.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useContext } from 'react';
-import Header from 'components/Header';
+// import Header from 'components/Header';
+import { Header, Select } from 'nhsuk-react-components';
 import Notification from 'components/Notification';
 import { AuthContext, PathwayContext } from 'app/context';
 import './pagelayout.css';
@@ -21,13 +22,30 @@ const PageLayout = ({
   const actualCurrentPathwayId = currentPathwayId || pathwayOptions[0].id;
   return (
     <div>
-      <Header
+      {/* <Header
         pathwayOptions={ pathwayOptions }
         currentPathwayId={ actualCurrentPathwayId }
         pathwayOnItemSelect={ () => console.log('item selected') }
         searchOnSubmit={ () => console.log('search submit') }
         usersName={ `${user?.firstName} ${user?.lastName}` }
-      />
+      /> */}
+      <Header>
+        <Header.Container>
+          <Header.Content>
+            <Header.MenuToggle />
+            <Header.Search />
+          </Header.Content>
+        </Header.Container>
+        <Header.Nav>
+          <Header.NavItem href="/" mobileOnly>
+            Home
+          </Header.NavItem>
+          <Header.NavItem href="">Home</Header.NavItem>
+          <Header.NavItem href="mdt">MDT</Header.NavItem>
+          <Header.NavItem href="add-patient" disabled>Add Patient</Header.NavItem>
+          <Header.NavItem href="logout">Logout ({`${user?.firstName} ${user?.lastName}`})</Header.NavItem>
+        </Header.Nav>
+      </Header>
       {children}
       <Notification />
     </div>
