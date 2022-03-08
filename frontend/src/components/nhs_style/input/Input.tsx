06d14dfd9e18c8939c8d68f79598df972adfe9be
Nick Ives <nickives@gmail.com>
2022-03-08 12:07:12 +0000

    Formatting improvement

--- frontend/src/components/nhs_style/input/Input.tsx
+++ frontend/src/components/nhs_style/input/Input.tsx
@@ -17,7 +17,9 @@ const Input = ({
 }: InputProps): JSX.Element => {
   const _className = error ? 'nhsuk-input nhsuk-input--error' : 'nhsuk-input';
   const { ref, ...rest } = register;
-  const _error = typeof error === 'string' ? <span className="nhsuk-error-message" id={ `${id}--error` } role="alert"><span className="nhsuk-u-visually-hidden">Error:</span>{ error }</span> : '';
+  const _error = typeof error === 'string'
+    ? <span className="nhsuk-error-message" id={ `${id}--error` } role="alert"><span className="nhsuk-u-visually-hidden">Error:</span>{ error }</span>
+    : '';
   return (
     <div className={ error ? 'nhsuk-form-group nhsuk-form-group--error' : 'nhsuk-form-group' }>
       {label ? <label className="nhsuk-label" id={ `${id}--label` } htmlFor={ id }>{ label }</label> : ''}
