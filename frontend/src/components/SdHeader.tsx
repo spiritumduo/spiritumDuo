5573c963b63458210d489294944c9859b60af8f6
Joe <joechanning@protonmail.com>
2022-03-08 21:44:29 +0000

    Corrected header components

--- frontend/src/components/SdHeader.tsx
+++ frontend/src/components/SdHeader.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { Header } from 'nhsuk-react-components';
+/* eslint-disable react/button-has-type */
+import React, { useState } from 'react';
+import { Header, SearchIcon, CloseIcon, Container } from 'nhsuk-react-components';
+import { List, ThreeDots } from 'react-bootstrap-icons';
 
 import User from '../types/Users';
 import PathwayOption from '../types/PathwayOption';
@@ -18,29 +20,66 @@ const SdHeader = ({
   pathwayOptions, currentPathwayId, pathwayOnItemSelect, searchOnSubmit, user,
 }: SdHeaderProps): JSX.Element => {
   const currentOption = pathwayOptions.find((p) => p.id === currentPathwayId);
+  const [searchState, setSearchState] = useState(false);
+  const [navbarState, setNavbarState] = useState(false);
   return (
     <Header>
       <Header.Container>
-        <Header.Content className="float-start w-100">
-          <a href="/app"><img src={ sdInvertedImage } alt="Spiritum Duo logo" className="nhsuk-header__logo" height="40px" /></a>
-          <Header.Search onSubmit={ (e) => searchOnSubmit(e) } />
-          <Header.MenuToggle />
-          <PathwaySelector
-            options={ pathwayOptions }
-            currentOption={ currentOption }
-            onItemSelect={ (name) => { pathwayOnItemSelect(name); } }
-          />
-        </Header.Content>
+        <a href="/app" className="me-2"><img src={ sdInvertedImage } alt="Spiritum Duo logo" className="nhsuk-header__logo" height="40px" /></a>
+        <button
+          className={ `nhsuk-header__menu-toggle position-relative float-end ms-1 ${navbarState ? 'is-active' : ''}` }
+          style={ { width: '40px', height: '40px', padding: '0', right: '0' } }
+          aria-label="Open search"
+          aria-expanded={ navbarState ? 'true' : 'false' }
+          onClick={ () => setNavbarState(!navbarState) }
+        >
+          <ThreeDots />
+        </button>
+        <button
+          className={`nhsuk-header__search-toggle float-end p-0 mx-1 position-relative ${searchState ? 'is-active' : ''}`}
+          style={{height: '40px', width: '40px'}}
+          aria-label="Open search"
+          aria-expanded={searchState ? 'true' : 'false'}
+          onClick={ () => setSearchState(!searchState) }
+        >
+          <SearchIcon />
+          <span className="nhsuk-u-visually-hidden">Search</span>
+        </button>
+        <PathwaySelector
+          options={ pathwayOptions }
+          currentOption={ currentOption }
+          onItemSelect={ (name) => { pathwayOnItemSelect(name); } }
+        />
+        <div className="nhsuk-header__search" aria-disabled>
+          <div className={`nhsuk-header__search-wrap ${searchState ? 'js-show' : ''}`}>
+            <form className="nhsuk-header__search-form" role="search">
+              <input className="nhsuk-search__input" id="searchInput" disabled />
+              <button className="nhsuk-search__submit" type="submit" disabled>
+                <SearchIcon />
+                <span className="nhsuk-u-visually-hidden">Search</span>
+              </button>
+              <button className="nhsuk-search__close">
+                <CloseIcon />
+                <span className="nhsuk-u-visually-hidden">Close search</span>
+              </button>
+            </form>
+          </div>
+        </div>
       </Header.Container>
-      <Header.Nav>
-        <Header.NavItem href="/" mobileOnly>
-          Home
-        </Header.NavItem>
-        <Header.NavItem style={ { fontSize: '1.1875rem' } } href="">Home</Header.NavItem>
-        <Header.NavItem style={ { fontSize: '1.1875rem' } } href="mdt">MDT</Header.NavItem>
-        <Header.NavItem style={ { fontSize: '1.1875rem' } } href="add-patient" disabled>Add Patient</Header.NavItem>
-        <Header.NavItem style={ { fontSize: '1.1875rem' } } href="logout">Logout ({`${user?.firstName} ${user?.lastName}`})</Header.NavItem>
-      </Header.Nav>
+
+      <nav className={ `nhsuk-header__navigation ${navbarState ? 'js-show' : ''}` }>
+        <Container>
+          <p className="nhsuk-header__navigation-title">
+            <span>Menu</span>
+          </p>
+          <ul className="nhsuk-header__navigation-list">
+            <Header.NavItem style={ { fontSize: '1.1875rem' } } href="">Home</Header.NavItem>
+            <Header.NavItem style={ { fontSize: '1.1875rem' } } href="mdt">MDT</Header.NavItem>
+            <Header.NavItem style={ { fontSize: '1.1875rem' } } href="add-patient" disabled>Add Patient</Header.NavItem>
+            <Header.NavItem style={ { fontSize: '1.1875rem' } } href="logout">Logout ({`${user?.firstName} ${user?.lastName}`})</Header.NavItem>
+          </ul>
+        </Container>
+      </nav>
     </Header>
   );
 };
