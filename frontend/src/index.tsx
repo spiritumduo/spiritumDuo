29b400d10387b7fe8e427aac557460e4aebb319c
Nick Ives <nickives@gmail.com>
2022-03-16 14:43:36 +0000

    Remove redundant update

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -33,10 +33,6 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
-navigator.serviceWorker.ready.then((registration) => {
-  console.log('updating service worker');
-  registration.update();
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
