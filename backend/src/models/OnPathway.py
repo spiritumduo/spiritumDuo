2d8df7046ad3449fcd13e1ef74bd4774b4ae710f
Joe <joechanning@protonmail.com>
2022-03-21 15:38:30 +0000

    Re-added `native_enum` parameter to `Enum`

--- backend/src/models/OnPathway.py
+++ backend/src/models/OnPathway.py
@@ -2,20 +2,39 @@ from . import db
 from sqlalchemy import func, false, Enum
 from SdTypes import DecisionTypes
 
+
 class OnPathway(db.Model):
     __tablename__ = "tbl_on_pathway"
 
     id = db.Column(db.Integer(), primary_key=True)
-    patient_id = db.Column(db.Integer(), db.ForeignKey('tbl_patient.id'), nullable=False)
-    pathway_id = db.Column(db.Integer(), db.ForeignKey('tbl_pathway.id'), nullable=False)
-    is_discharged = db.Column(db.Boolean(), server_default=false(), default=False, nullable=False)
+    patient_id = db.Column(
+        db.Integer(), db.ForeignKey('tbl_patient.id'), nullable=False
+    )
+    pathway_id = db.Column(
+        db.Integer(), db.ForeignKey('tbl_pathway.id'), nullable=False
+    )
+    is_discharged = db.Column(
+        db.Boolean(), server_default=false(), default=False, nullable=False
+    )
     awaiting_decision_type = db.Column(
-     Enum(DecisionTypes), default=DecisionTypes.TRIAGE.value,
-     server_default=DecisionTypes.TRIAGE.value, nullable=False
+        Enum(DecisionTypes, native_enum=False),
+        default=DecisionTypes.TRIAGE.value,
+        server_default=DecisionTypes.TRIAGE.value, nullable=False
     )
-    added_at = db.Column(db.DateTime(), server_default=func.now(), nullable=False)
+    added_at = db.Column(
+        db.DateTime(), server_default=func.now(), nullable=False
+    )
     updated_at = db.Column(
-        db.DateTime, server_default=func.now(), onupdate=func.now(), nullable=False
+        db.DateTime, server_default=func.now(), onupdate=func.now(),
+        nullable=False
     )
-    referred_at = db.Column(db.DateTime(), server_default=func.now(), nullable=False)
-    under_care_of_id = db.Column(db.Integer(), db.ForeignKey('tbl_user.id'), nullable=True)
+    referred_at = db.Column(
+        db.DateTime(), server_default=func.now(), nullable=False
+    )
+    under_care_of_id = db.Column(
+        db.Integer(), db.ForeignKey('tbl_user.id'), nullable=True
+    )
+    lock_user_id = db.Column(
+        db.Integer(), db.ForeignKey('tbl_user.id'), nullable=True
+    )
+    lock_end_time = db.Column(db.DateTime(), nullable=True)
