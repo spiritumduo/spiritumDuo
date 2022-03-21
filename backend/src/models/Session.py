b214eb03fdbd172781cdd3975e20b3ea8641fb45
Joe <joechanning@protonmail.com>
2022-03-21 15:38:09 +0000

    Linter refactor

--- backend/src/models/Session.py
+++ backend/src/models/Session.py
@@ -1,9 +1,14 @@
 from .db import db
 from sqlalchemy import func
 
+
 class Session(db.Model):
     __tablename__ = "tbl_session"
 
     session_key = db.Column(db.String(), primary_key=True)
-    user_id = db.Column(db.Integer(), db.ForeignKey('tbl_user.id'), nullable=False)
-    expiry = db.Column(db.DateTime(), server_default=func.now(), nullable=False)
+    user_id = db.Column(
+        db.Integer(), db.ForeignKey('tbl_user.id'), nullable=False
+    )
+    expiry = db.Column(
+        db.DateTime(), server_default=func.now(), nullable=False
+    )
