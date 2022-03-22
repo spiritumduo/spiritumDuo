0c18709b594f2a4f08bddf8e3875ce53d0e6a270
Joe <joechanning@protonmail.com>
2022-03-22 16:23:28 +0000

    Added ownership for `lockOnPathway`

--- backend/src/gql/mutation/lock_on_pathway.py
+++ backend/src/gql/mutation/lock_on_pathway.py
@@ -4,7 +4,7 @@ from authentication.authentication import needsAuthorization
 from graphql.type import GraphQLResolveInfo
 from datetime import datetime, timedelta
 from config import config
-
+from common import DataCreatorInputErrors
 
 @mutation.field("lockOnPathway")
 @needsAuthorization(["authenticated"])
@@ -13,6 +13,7 @@ async def resolve_lock_on_pathway(
     info: GraphQLResolveInfo = None,
     input: dict = None
 ) -> OnPathway:
+    errors: DataCreatorInputErrors = DataCreatorInputErrors()
     userId = int(info.context['request'].user.id)
     onPathwayId = int(input['onPathwayId'])
     unlock = ('unlock' in input and input['unlock']) or False
@@ -21,6 +22,17 @@ async def resolve_lock_on_pathway(
     ).gino.one()
 
     if not unlock:
+        if (
+            pathway.lock_end_time is not None and
+            pathway.lock_end_time > datetime.now() and
+            pathway.lock_user_id != userId
+        ):
+            errors.addError(
+                "lock_end_time",
+                "A lock is already in place by another user!"
+            )
+            return errors
+
         await pathway.update(
             lock_user_id=userId,
             lock_end_time=(
@@ -31,11 +43,16 @@ async def resolve_lock_on_pathway(
             )
         ).apply()
     else:
-        await pathway.update(
-            lock_user_id=None,
-            lock_end_time=None
-        ).apply()
+        if userId == pathway.lock_user_id:
+            await pathway.update(
+                lock_user_id=None,
+                lock_end_time=None
+            ).apply()
+        else:
+            errors.addError(
+                "lock_user_id",
+                "You cannot unlock a lock that belongs to you!"
+            )
+            return errors
 
-    return await OnPathway.query.where(
-        OnPathway.id == onPathwayId
-    ).gino.one()
+    return pathway
