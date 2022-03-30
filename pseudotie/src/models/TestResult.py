b5aba0b8e3215e1585dd4b7962249562ecedcc59
Joe <joechanning@protonmail.com>
2022-03-30 16:11:33 +0100

    formatting

--- pseudotie/src/models/TestResult.py
+++ pseudotie/src/models/TestResult.py
@@ -7,6 +7,8 @@ class TestResult(db.Model):
     __tablename__ = "tbl_test_result"
 
     id = db.Column(db.Integer(), primary_key=True)
+    patient_id = db.Column(
+        db.Integer(), db.ForeignKey('tbl_patient.id'), nullable=True)
     description = db.Column(db.String(), nullable=True)
     type_reference_name = db.Column(db.String(), nullable=False)
     current_state = db.Column(
@@ -18,5 +20,4 @@ class TestResult(db.Model):
     updated_at = db.Column(
         db.DateTime(), server_default=func.now(), nullable=False)
     planned_return_time = db.Column(
-        db.DateTime(), server_default=func.now(), nullable=True
-    )
+        db.DateTime(), server_default=func.now(), nullable=True)
