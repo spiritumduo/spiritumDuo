9d934ec6ceaf6e9e92cbeae352919f8680cdd218
Nick Ives <nickives@gmail.com>
2022-04-07 11:28:03 +0100

    Refactored to break up fixtures into independent units. Moved construction of `context` into the context fixture

--- backend/src/tests/conftest.py
+++ backend/src/tests/conftest.py
@@ -1,29 +1,23 @@
-import dataclasses
-import os
+# It's very important this happens first
+from starlette.config import environ
+environ['TESTING'] = "True"
 
+import asyncio
+from gino import GinoConnection
+import dataclasses
+from async_asgi_testclient import TestClient
 from gino_starlette import Gino
 from httpx import AsyncClient
 import pytest
 import pytest_asyncio
-from starlette.config import environ
 from unittest.mock import AsyncMock
 from bcrypt import hashpw, gensalt
-from models.db import db
+from models.db import db, TEST_DATABASE_URL
 from models import User, Pathway, MilestoneType
 from api import app
 from sqlalchemy_utils import database_exists, create_database, drop_database
 from trustadapter import TrustAdapter
 
-DB_STR = "postgresql://{user}:{password}@{host}:{port}/{database}"
-
-# The database has 'test_' prepended here
-TEST_DATABASE_URL = DB_STR.format(
-    host=os.getenv("DATABASE_HOSTNAME"),
-    port=os.getenv("DATABASE_PORT"),
-    user=os.getenv("DATABASE_USERNAME"),
-    password=os.getenv("DATABASE_PASSWORD"),
-    database="test_" + os.getenv("DATABASE_NAME"),
-)
 
 class ContextStorage:
     """
@@ -33,14 +27,21 @@ class ContextStorage:
 
 
 @pytest.fixture
-async def create_test_client():
+async def httpx_test_client():
     async with AsyncClient(
         app=app, base_url="http://localhost:8080"
     ) as client:
         yield client
 
+
 @pytest.fixture
-async def create_test_database() -> Gino:
+def test_client():
+    client = TestClient(app)
+    yield client
+
+
+@pytest.fixture
+async def create_test_database(event_loop) -> Gino:
     if database_exists(TEST_DATABASE_URL):
         drop_database(TEST_DATABASE_URL)
     create_database(TEST_DATABASE_URL)
@@ -49,19 +50,23 @@ async def create_test_database() -> Gino:
     yield engine
     drop_database(TEST_DATABASE_URL)
 
-@pytest.fixture
+
+@pytest.fixture(autouse=True)
 async def db_start_transaction(create_test_database):
     engine = create_test_database
-    conn = await engine.acquire()
+    conn: GinoConnection = await engine.acquire()
     tx = await conn.transaction()
     yield [conn, tx]
     await tx.rollback()
+    await conn.release()
+
 
 @pytest.fixture
 async def test_pathway() -> Pathway:
     return await Pathway.create(
         name="BRONCHIECTASIS",
     )
+
 
 @pytest.fixture
 async def test_milestone_type() -> MilestoneType:
@@ -71,13 +76,15 @@ async def test_milestone_type() -> MilestoneType:
         is_checkbox_hidden=True,
     )
 
+
 @dataclasses.dataclass
 class UserFixture:
     user: User
     password: str
 
+
 @pytest.fixture
-async def test_user(test_pathway) -> UserFixture:
+async def test_user(test_pathway, db_start_transaction) -> UserFixture:
     user_info = {
         "username": "testUser",
         "password": "testPassword"
@@ -100,16 +107,18 @@ async def test_user(test_pathway) -> UserFixture:
     )
 
 @pytest.fixture
-async def login_user(test_user, create_test_client):
-    client = create_test_client
+async def login_user(test_user: UserFixture, httpx_test_client):
+    client = httpx_test_client
     user_fixture = test_user
-    return await client.post(
+    res = await client.post(
         url='/rest/login/',
         json={
             "username": user_fixture.user.username,
             "password": user_fixture.password,
         }
     )
+    yield res
+
 
 @pytest.fixture
 async def mock_trust_adapter():
@@ -122,7 +131,7 @@ async def mock_trust_adapter():
 @pytest_asyncio.fixture(scope="function")
 async def context(
     create_test_database,
-    create_test_client,
+    httpx_test_client,
     db_start_transaction,
     mock_trust_adapter,
     test_pathway,
@@ -133,7 +142,7 @@ async def context(
     # At least now this construction is now done here, rather than being scattered across this module
     cs = ContextStorage()
     cs.engine = create_test_database
-    cs.client = create_test_client
+    cs.client = httpx_test_client
     conn, tx = db_start_transaction
     cs.conn = conn
     cs.tx = tx
