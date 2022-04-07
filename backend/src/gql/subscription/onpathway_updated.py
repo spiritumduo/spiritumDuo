ac2571c536ca9c9a2e8017f8bd29d5492f28a759
Joe Channing <jcfoxtrot54@protonmail.com>
2022-04-07 14:55:39 +0000

    added `includeDischarge` logic handling

--- backend/src/gql/subscription/onpathway_updated.py
+++ backend/src/gql/subscription/onpathway_updated.py
@@ -1,10 +1,6 @@
-# onPathwayUpdated
-import logging
 from typing import Any, AsyncGenerator
-
 from dependency_injector.wiring import Provide, inject
 from graphql import GraphQLResolveInfo
-
 from containers import SDContainer
 from models import OnPathway
 from .subscription_type import subscription
@@ -12,35 +8,30 @@ from .subscription_type import subscription
 
 @subscription.source("onPathwayUpdated")
 @inject
-async def milestone_resolved_generator(
+async def on_pathway_updated_generator(
     _: Any = None,
     info: GraphQLResolveInfo = None,
     pathwayId: int = None,
+    includeDischarged: bool = False,
     pub=Provide[SDContainer.pubsub_service]
 ) -> AsyncGenerator:
-    """
-        this needs to update on:
-            - decision point submission \/
-            - patient lock \/
-            - patient unlock \/
-            - import milestone \/
-            - test result completion
-    """
     topic = pub.subscribe("on-pathway-updated")
-    log = logging.getLogger("uvicorn")
-    log.debug(f"TOPIC: {topic}")
     async with topic as subscriber:
         async for on_pathway in subscriber:
-            if on_pathway.pathway_id == pathwayId:
-                log.info(
-                    f"ON_PATHWAY_UPDATED SUBSCRIPTION RESOLVER: {on_pathway}"
-                )
-                yield on_pathway
+            if int(on_pathway.pathway_id) == int(pathwayId):
+                if (
+                    (not includeDischarged and
+                        not on_pathway.is_discharged)
+                    or includeDischarged
+                ):
+                    yield on_pathway
 
 
 @subscription.field("onPathwayUpdated")
 async def on_pathway_updated_field(
     obj: OnPathway = None,
-    info: GraphQLResolveInfo = None
+    info: GraphQLResolveInfo = None,
+    pathwayId: int = None,
+    includeDischarged: bool = None,
 ):
     return obj
