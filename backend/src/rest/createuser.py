4a4b1b65470a9c80daff71708ccfabbf177cc176
Joe Channing <jcfoxtrot54@protonmail.com>
2022-04-13 00:30:34 +0000

    added `isAdmin`, removed password from return data

--- backend/src/rest/createuser.py
+++ backend/src/rest/createuser.py
@@ -1,8 +1,9 @@
+from models import User
 from .api import _FastAPI
 from fastapi import Request
 from pydantic import BaseModel
 from datacreators import CreateUser
-from authentication.authentication import needsAuthenticated
+from authentication.authentication import needsAuthorization
 
 
 class CreateUserInput(BaseModel):
@@ -12,17 +13,26 @@ class CreateUserInput(BaseModel):
     lastName: str
     department: str
     defaultPathwayId: int
+    isAdmin: bool
 
 
 @_FastAPI.post("/createuser/")
-@needsAuthenticated
+@needsAuthorization(["admin"])
 async def create_user(request: Request, input: CreateUserInput):
-    user = await CreateUser(
+    user: User = await CreateUser(
         username=input.username,
         password=input.password,
         first_name=input.firstName,
         last_name=input.lastName,
         department=input.department,
-        default_pathway_id=int(input.defaultPathwayId)
+        default_pathway_id=int(input.defaultPathwayId),
+        is_admin=input.isAdmin
     )
-    return user
+    return {
+        "username": user.username,
+        "firstName": user.first_name,
+        "lastName": user.last_name,
+        "department": user.department,
+        "defaultPathwayId": user.default_pathway_id,
+        "isAdmin": user.is_admin,
+    }
