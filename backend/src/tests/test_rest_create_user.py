5bde90aebb890f64fe1b3dafa633ea28f0e3b7b3
Joe Channing <jcfoxtrot54@protonmail.com>
2022-04-14 18:48:55 +0000

    fix test inline with new changes

--- backend/src/tests/test_rest_create_user.py
+++ backend/src/tests/test_rest_create_user.py
@@ -1,50 +1,90 @@
 import json
 import pytest
-from random import randint
-from hamcrest import assert_that, equal_to
+from hamcrest import assert_that, equal_to, has_item, not_
+from models.User import User
 
 
 # Feature: User account operations
 # Scenario: a new user needs to be added into the system
 @pytest.mark.asyncio
-async def test_create_user(context):
+async def test_create_user_correct(context):
     """
     When: we create their user account
     """
     NEW_CLINICIAN = {
         "firstName": "JOHN",
         "lastName": "SMITH",
-        "username": f"JOHN.SMITH{randint(1000,9999)}",
+        "username": "JOHN.SMITH123",
         "password": "VERYSECUREPASSWORD",
         "department": "ONCOLOGY",
+        "defaultPathwayId": context.PATHWAY.id,
+        "isActive": True
     }
 
     create_user_account = await context.client.post(
         url="rest/createuser/",
-        json={
-            "username": NEW_CLINICIAN['username'],
-            "password": NEW_CLINICIAN['password'],
-            "firstName": NEW_CLINICIAN['firstName'],
-            "lastName": NEW_CLINICIAN['lastName'],
-            "department": NEW_CLINICIAN['department'],
-            "defaultPathwayId": context.PATHWAY.id
-        }
+        json=NEW_CLINICIAN
     )
     assert_that(create_user_account.status_code, equal_to(200))
 
-    assert "error" not in json.loads(create_user_account.text)
-    user_result = json.loads(create_user_account.text)
+    query_result = json.loads(create_user_account.text)
+
+    assert_that(query_result, not_(has_item('error')))
+    assert_that(query_result, has_item('user'))
+
+    user_result = query_result['user']
 
     assert_that(user_result['username'], equal_to(NEW_CLINICIAN['username']))
     assert_that(
-        user_result['first_name'], equal_to(NEW_CLINICIAN['firstName'])
+        user_result['firstName'], equal_to(NEW_CLINICIAN['firstName'])
     )
     assert_that(
-        user_result['last_name'], equal_to(NEW_CLINICIAN['lastName'])
+        user_result['lastName'], equal_to(NEW_CLINICIAN['lastName'])
     )
     assert_that(
         user_result['department'], equal_to(NEW_CLINICIAN['department'])
     )
     assert_that(
-        user_result['default_pathway_id'], equal_to(context.PATHWAY.id)
+        user_result['defaultPathwayId'], equal_to(context.PATHWAY.id)
     )
+
+
+# Feature: User account operations
+# Scenario: a new user needs to be added into the system but
+# username already exists
+@pytest.mark.asyncio
+async def test_create_user_username_preexists(context):
+    """
+    When: we create their user account
+    """
+
+    await User.create(
+        first_name="Michael",
+        last_name="Myers",
+        department="Scary Dept",
+        username="michael",
+        password="thisshouldbeahash",
+        default_pathway_id=context.PATHWAY.id,
+        is_active=True
+    )
+
+    NEW_CLINICIAN = {
+        "firstName": "Michael",
+        "lastName": "Johnson",
+        "username": "michael",
+        "password": "VERYSECUREPASSWORD",
+        "department": "ONCOLOGY",
+        "defaultPathwayId": context.PATHWAY.id,
+        "isActive": True
+    }
+
+    create_user_account = await context.client.post(
+        url="rest/createuser/",
+        json=NEW_CLINICIAN
+    )
+    assert_that(create_user_account.status_code, equal_to(200))
+
+    query_result = json.loads(create_user_account.text)
+
+    assert_that(query_result, has_item('error'))
+    assert_that(query_result, not_(has_item('user')))
