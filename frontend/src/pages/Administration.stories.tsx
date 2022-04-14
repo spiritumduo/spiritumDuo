49ce9c601084859b3ce0fb65b758fc52da1320c6
Joe Channing <jcfoxtrot54@protonmail.com>
2022-04-14 20:08:10 +0000

    Updated test/stories

--- frontend/src/pages/Administration.stories.tsx
+++ frontend/src/pages/Administration.stories.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
+import fetchMock from 'fetch-mock';
 import User from 'types/Users';
 import { DefaultLayout } from 'components/PageLayout.stories';
 import PageLayout, { PageLayoutProps } from 'components/PageLayout';
@@ -38,13 +39,71 @@ export default {
   ],
 } as Meta<typeof AdministrationPage>;
 
-export const Default: Story = () => (
-  <AdministrationPage />
-);
-
-Default.args = {
-
+export type CreateUserReturnUser = {
+  username: string;
+  firstName: string;
+  lastName: string;
+  department: string;
+  defaultPathwayId: number;
+  isActive: boolean;
 };
-Default.parameters = {
 
+export type CreateUserReturnData = {
+  user: CreateUserReturnUser | null;
+  error: string | null;
+};
+
+/**
+ * Successful creation
+ */
+const successfulCreateAccountMock: CreateUserReturnData = {
+  user: {
+    firstName: 'John',
+    lastName: 'Doe',
+    username: 'jdoe',
+    department: 'Respiratory',
+    defaultPathwayId: 1,
+    isActive: false,
+  },
+  error: null,
+};
+
+/**
+ * User account already exists
+ */
+const usernameExistsCreateAccountMock: CreateUserReturnData = {
+  user: null,
+  error: 'an account with this username already exists',
+};
+
+/**
+ * Working state
+ */
+export const Default: Story = () => {
+  fetchMock.restore().mock('end:/rest/createuser', successfulCreateAccountMock);
+  return <AdministrationPage />;
+};
+
+/**
+ * Working state with loading delay
+ */
+export const Loading: Story = () => {
+  fetchMock.restore().mock('end:/rest/createuser', successfulCreateAccountMock, { delay: 1000 });
+  return <AdministrationPage />;
+};
+
+/**
+ * HTTP error state
+ */
+export const Error: Story = () => {
+  fetchMock.restore().mock('end:/rest/createuser', { body: { user: null, error: null }, status: 500 });
+  return <AdministrationPage />;
+};
+
+/**
+ * User account already exists
+ */
+export const UsernameAlreadyExists: Story = () => {
+  fetchMock.restore().mock('end:/rest/createuser', usernameExistsCreateAccountMock);
+  return <AdministrationPage />;
 };
