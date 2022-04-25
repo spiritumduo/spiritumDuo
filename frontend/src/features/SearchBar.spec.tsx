9b2b715906138989b18ddf02d06cc0e392466aa6
Nick Ives <nickives@gmail.com>
2022-04-25 14:05:01 +0100

    SearchBar tests

---
+++ frontend/src/features/SearchBar.spec.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+
+import { fireEvent, render, screen, waitFor, within } from '@testing-library/react';
+import { composeStories } from '@storybook/testing-react';
+import MockSdApolloProvider from 'test/mocks/mockApolloProvider';
+import userEvent from '@testing-library/user-event';
+import * as stories from './SearchBar.stories';
+
+const { Default } = composeStories(stories);
+
+const renderDefault = () => {
+  render(
+    <MockSdApolloProvider mocks={ Default.parameters?.apolloClient.mocks }>
+      <Default />
+    </MockSdApolloProvider>,
+  );
+};
+
+
+it('Should render properly', () => {
+  renderDefault();
+  expect(screen.getByRole('textbox', { name: /^search$/i })).toBeInTheDocument();
+  expect(screen.getByRole('button', { name: /^submit\s+search$/i })).toBeInTheDocument();
+});
