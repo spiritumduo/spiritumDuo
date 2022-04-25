ffb5c80b933ac6d4c6fb57bc0723bab9eb64be4c
Nick Ives <nickives@gmail.com>
2022-04-25 12:50:29 +0100

    Use new apollo mock

--- frontend/src/features/SearchBar.stories.tsx
+++ frontend/src/features/SearchBar.stories.tsx
@@ -3,62 +3,55 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react';
 
 import SearchBar, { PATIENT_SEARCH_QUERY } from 'features/SearchBar';
+import { MemoryRouter } from 'react-router';
+import { NewMockSdApolloProvider } from 'test/mocks/mockApolloProvider';
+
+const patients = [{
+  id: '1',
+  firstName: 'test',
+  lastName: 'test',
+  hospitalNumber: 'fMRN1234567',
+  nationalNumber: 'fNHS1234567890',
+}].flatMap((p) => {
+  const returnArray = [];
+  for (let i = 0; i < 5; ++i) {
+    returnArray.push({
+      id: i.toString(),
+      firstName: `${p.firstName} ${i}`,
+      lastName: `${p.lastName} ${i}`,
+      hospitalNumber: `${p.hospitalNumber}-${i}`,
+      nationalNumber: `${p.nationalNumber}-${i}`,
+    });
+  }
+  return returnArray;
+});
+
+const searchPatientMock = {
+  query: PATIENT_SEARCH_QUERY,
+  mockFn: () => Promise.resolve({
+    data: {
+      patientSearch: patients,
+    },
+  }),
+};
 
 export default {
   title: 'features/SearchBar',
   component: SearchBar,
+  decorators: [
+    (SearchBarStory) => (
+      <NewMockSdApolloProvider mocks={ [searchPatientMock] }>
+        <MemoryRouter>
+          <SearchBarStory />
+        </MemoryRouter>
+      </NewMockSdApolloProvider>
+    ),
+  ],
 } as Meta<typeof SearchBar>;
 
 const Template: Story = () => <SearchBar />;
 
 export const Default = Template.bind({});
-
-export const WithResults = Template.bind({});
-WithResults.parameters = {
-  apolloClient: {
-    mocks: [
-      {
-        request: {
-          query: PATIENT_SEARCH_QUERY,
-          variables: {
-            query: 'test',
-          },
-        },
-        result: {
-          data: {
-            patientSearch: [
-              {
-                id: '1',
-                firstName: 'test 1',
-                lastName: 'test 1',
-                hospitalNumber: 'fMRN1234567',
-                nationalNumber: 'fNHS1234567890',
-              },
-              {
-                id: '2',
-                firstName: 'test 2',
-                lastName: 'test 2',
-                hospitalNumber: 'fMRN1234567',
-                nationalNumber: 'fNHS1234567890',
-              },
-              {
-                id: '3',
-                firstName: 'test 3',
-                lastName: 'test 3',
-                hospitalNumber: 'fMRN1234567',
-                nationalNumber: 'fNHS1234567890',
-              },
-              {
-                id: '4',
-                firstName: 'test 3',
-                lastName: 'test 3',
-                hospitalNumber: 'fMRN1234567',
-                nationalNumber: 'fNHS1234567890',
-              },
-            ],
-          },
-        },
-      },
-    ],
-  },
+Default.parameters = {
+  patientMock: patients,
 };
