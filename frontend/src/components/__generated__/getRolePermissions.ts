c43300c659b8d3b7279ad9c0e2ed5baaebbc49db
Joe Channing <jcfoxtrot54@protonmail.com>
2022-04-27 13:10:21 +0000

    generated typings

---
+++ frontend/src/components/__generated__/getRolePermissions.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: getRolePermissions
+// ====================================================
+
+export interface getRolePermissions_getRolePermissions {
+  __typename: "RolePermission";
+  name: string;
+}
+
+export interface getRolePermissions {
+  getRolePermissions: getRolePermissions_getRolePermissions[] | null;
+}
