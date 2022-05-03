a4e1f4e21283e747cee6a82d1d22f4a1d93e465f
Joe Channing <jcfoxtrot54@protonmail.com>
2022-05-03 22:15:42 +0000

    Added/updated pathway operation mutations

--- backend/src/gql/mutation/__init__.py
+++ backend/src/gql/mutation/__init__.py
@@ -4,4 +4,7 @@ from .create_pathway import mutation
 from .create_decision_point import mutation
 from .import_milestone import mutation
 from .lock_on_pathway import mutation
+from .update_pathway import mutation
+from .delete_pathway import mutation
+
 type_list = [mutation]
--- backend/src/gql/mutation/update_pathway.py
+++ backend/src/gql/mutation/update_pathway.py
@@ -1,20 +1,21 @@
 from SdTypes import Permissions
+from dataupdaters import UpdatePathway
 from models import Pathway
 from .mutation_type import mutation
-from datacreators import CreatePathway
 from authentication.authentication import needsAuthorization
 from graphql.type import GraphQLResolveInfo
 
 
-@mutation.field("createPathway")
-@needsAuthorization([Permissions.PATHWAY_CREATE])
-async def resolve_create_pathway(
+@mutation.field("updatePathway")
+@needsAuthorization([Permissions.PATHWAY_UPDATE])
+async def resolve_update_pathway(
     obj=None,
     info: GraphQLResolveInfo = None,
     input: dict = None,
 ) -> Pathway:
-    return await CreatePathway(
+    return await UpdatePathway(
         context=info.context,
+        id=input['id'],
         name=input['name'],
         milestone_types=input['milestoneTypes'] if 'milestoneTypes' in input else None
     )
