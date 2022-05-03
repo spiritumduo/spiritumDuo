1d23cafd05b7dc9b1faea1d12de0b6bd757fe5ac
Joe Channing <jcfoxtrot54@protonmail.com>
2022-05-03 22:15:56 +0000

    added `pathwayId` to `getMilestoneTypes`

--- backend/src/gql/query/get_milestone_types.py
+++ backend/src/gql/query/get_milestone_types.py
@@ -1,5 +1,5 @@
 from .query_type import query
-from dataloaders import MilestoneTypeLoader
+from dataloaders import MilestoneTypeLoader, MilestoneTypeLoaderByPathwayId
 from authentication.authentication import needsAuthorization
 from graphql.type import GraphQLResolveInfo
 from SdTypes import Permissions
@@ -9,6 +9,11 @@ from SdTypes import Permissions
 @needsAuthorization([Permissions.MILESTONE_TYPE_READ])
 async def resolve_get_milestone_types(
     obj=None,
-    info: GraphQLResolveInfo = None
+    info: GraphQLResolveInfo = None,
+    pathwayId: int = None
 ):
-    return await MilestoneTypeLoader.load_all(info.context)
+    if pathwayId is None:
+        return await MilestoneTypeLoader.load_all(info.context)
+    else:
+        return await MilestoneTypeLoaderByPathwayId.load_from_id(
+            info.context, pathwayId)
