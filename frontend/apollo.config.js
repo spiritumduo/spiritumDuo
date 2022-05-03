087030bc9179d43fce44adac44179133176ccb99
Nick Ives <nickives@gmail.com>
2022-05-03 10:28:39 +0100

    Update service location

--- frontend/apollo.config.js
+++ frontend/apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     service: {
       name: 'Spiritum Duo',
-      url: '/api/graphql/',
+      url: 'http://localhost:8080/api/graphql/',
     },
     excludes: ['**/__generated__/**', '**/schema.graphql'],
   },
