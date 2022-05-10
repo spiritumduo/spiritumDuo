aeca928ed7290a3116964b8a80661f880b479ffe
Joe Channing <jcfoxtrot54@protonmail.com>
2022-05-10 15:28:52 +0000

    Remove unused user object

--- frontend/src/pages/Administration.stories.tsx
+++ frontend/src/pages/Administration.stories.tsx
@@ -1,20 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
-import fetchMock from 'fetch-mock';
-import User from 'types/Users';
 import { MemoryRouter } from 'react-router';
 import AdministrationPage from './Administration';
-
-const user: User = {
-  id: '1',
-  firstName: 'John',
-  lastName: 'Doe',
-  department: 'Respiratory',
-  defaultPathwayId: '1',
-  roles: [],
-  token: 'token',
-};
 
 export default {
   title: 'Pages/Administration',
