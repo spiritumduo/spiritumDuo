05174857921b93c0e1863732bd9abc5ce9b5538e
Joe <jcfoxtrot54@protonmail.com>
2022-05-12 16:33:27 +0000

    Add pathways to users

--- frontend/src/features/AdminUser/AdminUserUpdate.tsx
+++ frontend/src/features/AdminUser/AdminUserUpdate.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 import { useQuery, gql } from '@apollo/client';
 import { userAdminGetUserWithRoles } from './__generated__/userAdminGetUserWithRoles';
-import AdminUserForm, { GET_ROLES_FRAGMENT, GET_USER_FRAGMENT } from './components/AdminUserForm';
+import AdminUserForm, { GET_ROLES_FRAGMENT, GET_USER_FRAGMENT, GET_PATHWAYS_FRAGMENT } from './components/AdminUserForm';
 
 export const USER_ADMIN_GET_USER_WITH_ROLES_QUERY = gql`
   ${GET_USER_FRAGMENT}
   ${GET_ROLES_FRAGMENT}
+  ${GET_PATHWAYS_FRAGMENT}
   query userAdminGetUserWithRoles($id: ID!) {
     getUser(id: $id) {
       ...UserParts
     }
     getRoles {
       ...RoleParts
+    }
+    getPathways{
+      ...PathwayParts
     }
   }
 `;
@@ -33,6 +37,7 @@ export const AdminUserUpdate = ({ updateUserId }: AdminUserUpdateProps): JSX.Ele
     <AdminUserForm
       editUser={ data?.getUser ? data.getUser : undefined }
       roles={ data?.getRoles }
+      pathways={ data?.getPathways }
     />
   );
 };
