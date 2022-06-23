9ef98acdc55fb8f700d64b74f4ebbf924fec6de8
Joe Channing <jcfoxtrot54@protonmail.com>
2022-06-23 17:55:36 +0000

    Table display list of MDTs connection

--- frontend/src/pages/MDT.tsx
+++ frontend/src/pages/MDT.tsx
@@ -1,15 +1,97 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 
-import { Button, Container, Table } from 'nhsuk-react-components';
+import { Button, Container, ErrorMessage, Table } from 'nhsuk-react-components';
 import ReactPaginate from 'react-paginate';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
 import '../components/patientlist.css';
 import MdtManagement from 'features/MdtManagement/MdtManagement';
+import { gql, useQuery } from '@apollo/client';
+import { PathwayContext } from 'app/context';
+import edgesToNodes from 'app/pagination';
+import LoadingSpinner from 'components/LoadingSpinner/LoadingSpinner';
+import { getMdtConnectionQuery } from './__generated__/getMdtConnectionQuery';
+
+export const GET_MDT_CONNECTION_QUERY = gql`
+  query getMdtConnectionQuery(
+    $first: Int, $after: String, $pathwayId: ID!
+  ) {
+    getMdtConnection(
+      first: $first, after: $after, pathwayId: $pathwayId
+    ) @connection(
+        key: "getMdtConnection",
+        filter: ["pathwayId"]
+        )
+      {
+      totalCount
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      edges {
+        cursor
+        node {
+          id
+          pathway{
+            id
+            name
+          }
+          creator{
+            id
+            username
+            firstName
+            lastName
+          }
+          createdAt
+          plannedAt
+          updatedAt
+          location
+        }
+      }
+    }
+  }
+`;
+
+const useGetMdtConnectionQuery = (
+  pathwayId: string, first: number, after?: string,
+) => useQuery<getMdtConnectionQuery>(
+  GET_MDT_CONNECTION_QUERY, {
+    variables: {
+      pathwayId: pathwayId,
+      first: first,
+      after: after,
+    },
+  },
+);
 
 const MDTPage = (): JSX.Element => {
   const [showManageMdtModal, setShowManageMdtModal] = useState<boolean>(false);
+  const { currentPathwayId } = useContext(PathwayContext);
+  const [maxFetchedPage, setMaxFetchedPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+  const {
+    loading,
+    error,
+    data,
+    fetchMore,
+  } = useGetMdtConnectionQuery(currentPathwayId ? currentPathwayId?.toString() : '0', 10);
 
+  const { nodes, pageCount, pageInfo } = edgesToNodes<getMdtConnectionQuery['getMdtConnection']['edges'][0]['node']>(
+    data?.getMdtConnection, currentPage, 10,
+  );
+
+  let tableElements: { id: string; plannedAt: Date; location: string; }[] = [];
+
+  if (nodes) {
+    tableElements = nodes.flatMap((node) => {
+      if (!node) return [];
+      return {
+        id: node.id,
+        plannedAt: node.plannedAt,
+        location: node.location,
+      };
+    });
+  }
   return (
     <Container>
       <MdtManagement showModal={ showManageMdtModal } setShowModal={ setShowManageMdtModal } />
@@ -19,24 +101,35 @@ const MDTPage = (): JSX.Element => {
           <Tab>MDT by date</Tab>
         </TabList>
         <TabPanel>
-          <Table responsive role="grid" aria-describedby="pt_todo_list_aria" aria-label="patient list">
-            <Table.Head>
-              <Table.Row>
-                <Table.Cell>Date</Table.Cell>
-                <Table.Cell>Clinicians present</Table.Cell>
-                <Table.Cell>Location</Table.Cell>
-                <Table.Cell>Number of patients</Table.Cell>
-              </Table.Row>
-            </Table.Head>
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell>Thing</Table.Cell>
-                <Table.Cell>Thing</Table.Cell>
-                <Table.Cell>Thing</Table.Cell>
-                <Table.Cell>Thing</Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          </Table>
+          <LoadingSpinner loading={ loading }>
+            {
+              error
+                ? <ErrorMessage>{error.message}</ErrorMessage>
+                : ''
+            }
+            <Table responsive role="grid" aria-describedby="pt_todo_list_aria" aria-label="patient list">
+              <Table.Head>
+                <Table.Row>
+                  <Table.Cell>Date</Table.Cell>
+                  <Table.Cell>Clinicians present</Table.Cell>
+                  <Table.Cell>Location</Table.Cell>
+                  <Table.Cell>Number of patients</Table.Cell>
+                </Table.Row>
+              </Table.Head>
+              <Table.Body>
+                {
+                  tableElements.map((element) => (
+                    <Table.Row key={ element.id } className="active">
+                      <Table.Cell>{new Date(element.plannedAt).toLocaleDateString()}</Table.Cell>
+                      <Table.Cell>TODO: clinicians present</Table.Cell>
+                      <Table.Cell>{element.location}</Table.Cell>
+                      <Table.Cell>TODO: number of patients</Table.Cell>
+                    </Table.Row>
+                  ))
+                }
+              </Table.Body>
+            </Table>
+          </LoadingSpinner>
         </TabPanel>
       </Tabs>
       <br />
@@ -45,10 +138,22 @@ const MDTPage = (): JSX.Element => {
         nextLabel="next"
         breakLabel="..."
         breakClassName="break-me"
-        pageCount={ Math.ceil(2) }
+        pageCount={ pageCount }
         marginPagesDisplayed={ 2 }
         pageRangeDisplayed={ 5 }
-        onPageChange={ () => ({}) }
+        onPageChange={ (({ selected }) => {
+          setCurrentPage(selected);
+          if (selected > maxFetchedPage) {
+            if (pageInfo?.hasNextPage) {
+              setMaxFetchedPage(selected);
+              fetchMore({
+                variables: {
+                  after: pageInfo?.endCursor,
+                },
+              });
+            }
+          }
+        }) }
         containerClassName="pagination justify-content-center"
         activeClassName="bkcolour-nhs-blue"
         pageClassName="page-item"
