ce4f20cec9b0f7cdf4e6a439bfa29edcc6abe59f
Nick Ives <nickives@gmail.com>
2022-07-04 09:51:55 +0100

    Testing for GRP list query

--- frontend/src/features/PatientPathwayList/PatientPathwayList.spec.tsx
+++ frontend/src/features/PatientPathwayList/PatientPathwayList.spec.tsx
@@ -1,4 +1,16 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import { composeStories } from '@storybook/testing-react';
+import { render, waitFor } from '@testing-library/react';
+import * as stories from './PatientPathwayList.stories';
 
-const thing = { };
+const { Default } = composeStories(stories);
+
+it('It should render Default correctly', () => {
+  const { container } = render(<Default />);
+  expect(container).toMatchSnapshot();
+});
+
+it('Should have accessible table', async () => {
+  const { getByRole } = render(<Default />);
+  await waitFor(() => expect(getByRole('table', { name: /pathway\s+table/i })).toBeInTheDocument());
+});
