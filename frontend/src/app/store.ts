bca3bc43e3082bc7ae2fe1ce44bce68dd3df8780
Joe Channing <jcfoxtrot54@protonmail.com>
2022-07-04 14:08:31 +0000

    added `setOnMdtWorkflow` dispatch utlity

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import homePageReducer from 'pages/HomePage.slice';
 import modalPatientReducer from 'components/ModalPatient.slice';
 import searchBarReducer from 'features/SearchBar/SearchBar.slice';
+import onMdtWorkflowReducer from 'features/DecisionPoint/DecisionPoint.slice';
 
 const store = configureStore({
   reducer: {
     modalPatient: modalPatientReducer,
     homePage: homePageReducer,
     searchBar: searchBarReducer,
+    onMdtWorkflow: onMdtWorkflowReducer,
   },
 });
 
