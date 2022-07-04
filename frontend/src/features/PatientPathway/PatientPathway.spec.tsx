ec8f025d6d07f16faa77d8fbd7a84b8366fb566d
Nick Ives <nickives@gmail.com>
2022-07-04 09:52:53 +0100

    Single patient GRP query test

---
+++ frontend/src/features/PatientPathway/PatientPathway.spec.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { composeStories } from '@storybook/testing-react';
+import { render, waitFor } from '@testing-library/react';
+import * as stories from './PatientPathway.stories';
+
+const { Default } = composeStories(stories);
+
+it('It should render Default correctly', () => {
+  const { container } = render(<Default />);
+  expect(container).toMatchSnapshot();
+});
+
+it('Should have accessible table', async () => {
+  const { getByRole } = render(<Default />);
+  await waitFor(() => expect(getByRole('table', { name: /pathway\s+table/i })).toBeInTheDocument());
+});
