fe03881f8373cae35778a35f5b018a02195c0193
Joe Channing <jcfoxtrot54@protonmail.com>
2022-07-06 17:59:15 +0000

    Add other parameters onto getonmdtconn

--- backend/src/gql/query/get_on_mdt_connection.py
+++ backend/src/gql/query/get_on_mdt_connection.py
@@ -4,7 +4,7 @@ from .pagination import make_connection, validate_parameters
 from authentication.authentication import needsAuthorization
 from graphql.type import GraphQLResolveInfo
 from SdTypes import Permissions
-from models import OnMdt
+from models import OnMdt, Patient, MDT
 from models.db import db
 from .query_type import query
 
@@ -13,16 +13,25 @@ from .query_type import query
 @needsAuthorization([Permissions.MDT_READ, Permissions.PATIENT_READ])
 async def get_mdt_connection(
     obj=None, info: GraphQLResolveInfo = None, first=None,
-    after=None, last=None, before=None, mdtId=None
+    after=None, last=None, before=None, mdtId=None,
+    patientId=None, pathwayId=None
 ):
     validate_parameters(first, after, last, before)
 
+    if not mdtId and not (patientId and pathwayId):
+        return None
+
+    if mdtId:
+        query: str = OnMdt.query.where(OnMdt.mdt_id == int(mdtId))
+    else:
+        query: str = Patient.join(OnMdt, Patient.id == OnMdt.patient_id)\
+            .join(MDT, OnMdt.mdt_id == MDT.id).select()\
+            .where(Patient.id == int(patientId))\
+            .where(MDT.pathway_id == int(pathwayId))\
+            .execution_options(loader=OnMdt)
+
     async with db.acquire(reuse=False) as conn:
-        on_mdt_list: List[OnMdt] = await conn.all(
-            OnMdt.query.where(
-                OnMdt.mdt_id == int(mdtId)
-            )
-        )
+        on_mdt_list: List[OnMdt] = await conn.all(query)
     for on_mdt in on_mdt_list:
         OnMdtByIdLoader.prime(on_mdt.id, on_mdt, context=info.context)
 
