dbd383359299f1ed0177b8c415894cf71e510672
Nick Ives <nickives@gmail.com>
2022-07-12 15:49:14 +0100

    Fix generated types

--- frontend/src/pages/__generated__/createMdt.ts
+++ frontend/src/pages/__generated__/createMdt.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { MdtInput } from "./../../../__generated__/globalTypes";
+import { MdtInput } from "./../../__generated__/globalTypes";
 
 // ====================================================
 // GraphQL mutation operation: createMdt
--- frontend/src/pages/__generated__/getPatientForMdt.ts
+++ frontend/src/pages/__generated__/getPatientForMdt.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { PatientCommunicationMethods } from "./../../../__generated__/globalTypes";
+import { PatientCommunicationMethods } from "./../../__generated__/globalTypes";
 
 // ====================================================
 // GraphQL query operation: getPatientForMdt
