d60d48ae8084d35d48668805254d44874394856a
Joe Channing <jcfoxtrot54@protonmail.com>
2022-07-18 13:33:47 +0000

    Update generated types (fix CI failures), issue wrt moving global types to src

--- frontend/src/features/MdtManagement/tabpages/__generated__/updateMdt.ts
+++ frontend/src/features/MdtManagement/tabpages/__generated__/updateMdt.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { UpdateMdtInput } from "./../../../../__generated__/globalTypes";
+import { UpdateMdtInput } from "./../../../../../__generated__/globalTypes";
 
 // ====================================================
 // GraphQL mutation operation: updateMdt
--- frontend/src/features/PathwayManagement/tabpages/__generated__/updatePathway.ts
+++ frontend/src/features/PathwayManagement/tabpages/__generated__/updatePathway.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { UpdatePathwayInput } from "./../../../../__generated__/globalTypes";
+import { UpdatePathwayInput } from "./../../../../../__generated__/globalTypes";
 
 // ====================================================
 // GraphQL mutation operation: updatePathway
