d1db71da688afe7cb23d3f2ecce2110ebfe738a0
Joe Channing <jcf@foxtrot-titan.co.uk>
2022-07-19 18:36:36 +0000

    selenium run script

---
+++ selenium_tests/run_selenium_tests.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+import sys
+
+
+def run_tests():
+    subprocess.run("python3 -m pytest", shell=True)
+
+
+if __name__ == "__main__":
+    arg = (sys.argv[1] or "").lower()
+
+    if arg == "firefox":
+        print("Running Pytest/Selenium with Firefox browser client")
+        os.environ['SELENIUM_BROWSER_CLIENT'] = 'firefox'
+        run_tests()
+    elif arg == "chromium":
+        print("Running Pytest/Selenium with Chromium browser client")
+        os.environ['SELENIUM_BROWSER_CLIENT'] = 'chromium'
+        run_tests()
+    else:
+        print("Running Pytest/Selenium with Firefox browser client")
+        os.environ['SELENIUM_BROWSER_CLIENT'] = 'firefox'
+        run_tests()
+
+        print("Running Pytest/Selenium with Chromium browser client")
+        os.environ['SELENIUM_BROWSER_CLIENT'] = 'chromium'
+        run_tests()
