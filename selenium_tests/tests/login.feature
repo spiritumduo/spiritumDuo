9353ed679cac44a5c25d2eb93f268d2d486b65be
Joe Channing <jcf@foxtrot-titan.co.uk>
2022-07-19 17:18:19 +0000

    expands login testing to incorrect creds too

--- selenium_tests/tests/login.feature
+++ selenium_tests/tests/login.feature
@@ -3,9 +3,10 @@ Feature: Login
         Given the login page is displayed
         When I insert a correct username and password and submit
         Then I should be assigned a session cookie
+        Then I should not be on the login page
 
     Scenario: Logging the user in with invalid credentials
         Given the login page is displayed
-        When I insert an incorrect username and password
-        And I press the submit button
-        Then I should be assigned a session cookie
+        When I insert an incorrect username and password and submit
+        Then I should not be assigned a session cookie
+        And I should still be on the login page
--- selenium_tests/tests/test_login.py
+++ selenium_tests/tests/test_login.py
@@ -5,7 +5,7 @@ from pytest_bdd import scenario, given, when, then
 from time import sleep
 
 @scenario("login.feature", "Logging the user in with valid credentials")
-def test_login():
+def test_login_correct_creds():
     pass
 
 @given("the login page is displayed")
@@ -30,3 +30,41 @@ def check_cookie(firefox_driver: webdriver.Firefox):
 
     # check session cookie has been set  
     assert firefox_driver.get_cookie("SDSESSION") is not None
+
+@then("I should not be on the login page")
+def check_url(firefox_driver: webdriver.Firefox):
+    # check URL does not container /app/login  
+    assert firefox_driver.current_url.find("/login") == -1
+
+
+@scenario("login.feature", "Logging the user in with invalid credentials")
+def test_login_incorrect_creds():
+    pass
+
+@given("the login page is displayed")
+def set_login_page(firefox_driver: webdriver.Firefox):
+    firefox_driver.get("http://sd.foxtrot-titan.co.uk/app")
+
+@when("I insert an incorrect username and password and submit")
+def insert_correct_credentials(firefox_driver: webdriver.Firefox):
+    # username field
+    firefox_driver.find_element(By.NAME, "username").send_keys("thisisincorrect")
+
+    # password field
+    firefox_driver.find_element(By.NAME, "password").send_keys("thisiswrongtoo")    
+
+    # submit button
+    firefox_driver.find_element(By.ID, "submit").send_keys(Keys.ENTER)
+
+@then("I should not be assigned a session cookie")
+def check_cookie(firefox_driver: webdriver.Firefox):
+    # we wait what we think is a timely amount of time
+    sleep(1)
+
+    # check session cookie has been set  
+    assert firefox_driver.get_cookie("SDSESSION") is None
+
+@then("I should still be on the login page")
+def check_url(firefox_driver: webdriver.Firefox):
+    # check URL contains /app/login  
+    assert firefox_driver.current_url.find("/login") != -1
