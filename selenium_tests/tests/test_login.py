e5f963724db5539c7de2f12f844f9003e81340b3
Joe Channing <jcf@foxtrot-titan.co.uk>
2022-07-20 12:06:09 +0000

    fix typing, change domain

--- selenium_tests/tests/test_login.py
+++ selenium_tests/tests/test_login.py
@@ -11,12 +11,12 @@ def test_login_correct_creds():
 
 
 @given("the login page is displayed")
-def set_login_page_for_correct_credentials_test(driver: webdriver):
+def set_login_page_for_correct_credentials_test(driver: webdriver.Remote):
     driver.get("http://sd.foxtrot-titan.co.uk/app")
 
 
 @when("I insert a correct username and password and submit")
-def insert_correct_credentials(driver: webdriver):
+def insert_correct_credentials(driver: webdriver.Remote):
     # username field
     driver.find_element(By.NAME, "username").send_keys("demo-1-2")
 
@@ -28,7 +28,7 @@ def insert_correct_credentials(driver: webdriver):
 
 
 @then("I should be assigned a session cookie")
-def check_cookie_present(driver: webdriver):
+def check_cookie_present(driver: webdriver.Remote):
     # we wait what we think is a timely amount of time
     sleep(1)
 
@@ -37,7 +37,7 @@ def check_cookie_present(driver: webdriver):
 
 
 @then("I should not be on the login page")
-def check_url_is_not_login(driver: webdriver):
+def check_url_is_not_login(driver: webdriver.Remote):
     # check URL does not container /app/login
     assert driver.current_url.find("/login") == -1
 
@@ -48,12 +48,12 @@ def test_login_incorrect_creds():
 
 
 @given("the login page is displayed")
-def set_login_page_for_incorrect_credentials_test(driver: webdriver):
-    driver.get("http://sd.foxtrot-titan.co.uk/app")
+def set_login_page_for_incorrect_credentials_test(driver: webdriver.Remote):
+    driver.get("http://knightlx/app")
 
 
 @when("I insert an incorrect username and password and submit")
-def insert_incorrect_credentials(driver: webdriver):
+def insert_incorrect_credentials(driver: webdriver.Remote):
     # username field
     driver.find_element(By.NAME, "username").send_keys("thisisincorrect")
 
@@ -65,7 +65,7 @@ def insert_incorrect_credentials(driver: webdriver):
 
 
 @then("I should not be assigned a session cookie")
-def check_cookie_not_present(driver: webdriver):
+def check_cookie_not_present(driver: webdriver.Remote):
     # we wait what we think is a timely amount of time
     sleep(1)
 
@@ -74,6 +74,6 @@ def check_cookie_not_present(driver: webdriver):
 
 
 @then("I should still be on the login page")
-def check_url_is_login(driver: webdriver):
+def check_url_is_login(driver: webdriver.Remote):
     # check URL contains /app/login
     assert driver.current_url.find("/login") != -1
