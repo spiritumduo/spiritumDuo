f3a9165e85f170485a6f0a87eb77a71c2466b7c4
Joe Channing <jcf@foxtrot-titan.co.uk>
2022-07-20 19:37:22 +0000

    Re-configure Windows runner

--- selenium_tests/conftest.py
+++ selenium_tests/conftest.py
@@ -44,7 +44,7 @@ def driver():
     browser_choice: str = (
         'SELENIUM_BROWSER_CLIENT' in environ
         and environ['SELENIUM_BROWSER_CLIENT']
-        or 'chromium'
+        or 'edge'
     ).lower()
 
     if browser_choice == "firefox":
@@ -87,9 +87,23 @@ def driver():
 
     elif browser_choice == "edge":
         options = EdgeOptions()
-        driver = webdriver.Edge(EdgeService(
-            EdgeChromiumDriverManager.install()
-        ))
+
+        options.add_argument("--no-sandbox")
+        options.add_argument("--headless")
+        options.add_argument("--disable-setuid-sandbox")
+        options.add_argument("--remote-debugging-port=9222")
+        options.add_argument("--disable-dev-shm-using")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--start-maximized")
+        options.add_argument("--disable-infobars")
+        options.add_argument("--ignore-certificate-errors")
+
+        driver = webdriver.Edge(
+            service=EdgeService(EdgeChromiumDriverManager().install()),
+            options=options,
+        )
+
         driver.set_window_size(1920, 1080)
         driver.maximize_window()
 
