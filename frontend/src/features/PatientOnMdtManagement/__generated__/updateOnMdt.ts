688cb3964beb9eccf40dcbdd045881fa32476074
Joe Channing <jcf@foxtrot-titan.co.uk>
2022-07-21 14:27:03 +0000

    Re-generate types into src

--- frontend/src/features/PatientOnMdtManagement/__generated__/updateOnMdt.ts
+++ frontend/src/features/PatientOnMdtManagement/__generated__/updateOnMdt.ts
@@ -3,7 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
-import { UpdateOnMdtInput } from "./../../../../__generated__/globalTypes";
+import { UpdateOnMdtInput } from "./../../../__generated__/globalTypes";
 
 // ====================================================
 // GraphQL mutation operation: updateOnMdt
