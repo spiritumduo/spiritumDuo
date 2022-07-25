aa7f465aeff35fef8bb769c3b66ce9e54c333cae
Joe Channing <jcf@foxtrot-titan.co.uk>
2022-07-25 17:36:35 +0100

    remove `importclinicalrequest` as unused

--- backend/src/datacreators/__init__.py
+++ backend/src/datacreators/__init__.py
@@ -2,6 +2,5 @@ from .patient import CreatePatient
 from .pathway import CreatePathway
 from .decision_point import CreateDecisionPoint
 from .user import CreateUser
-from .clinical_request import ImportClinicalRequest
 from .role import create_role
 from .mdt import CreateMDT
