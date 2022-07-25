51c85694991a658bafbe6c0b99f0f61205aa9b20
Joe Channing <jcf@foxtrot-titan.co.uk>
2022-07-25 19:29:23 +0100

    fix update result logic

--- backend/src/rest/update_test_result.py
+++ backend/src/rest/update_test_result.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 from models import ClinicalRequest, OnPathway
 from fastapi.responses import Response
 from config import config
-
+from SdTypes import ClinicalRequestState
 
 class TestResultUpdate(BaseModel):
     id: str = None
@@ -23,16 +23,27 @@ async def update_test_result(
     if ('SDTIEKEY' not in request.cookies
             or request.cookies['SDTIEKEY'] != config['UPDATE_ENDPOINT_KEY']):
         return Response(status_code=401)
+    
     clinical_request: ClinicalRequest = await ClinicalRequest.query.where(
         ClinicalRequest.test_result_reference_id == str(data.id)
     ).gino.one_or_none()
-    await clinical_request.update(
-        current_state=data.new_state,
-        completed_at=datetime.now()
-    ).apply()
+
+    if data.new_state == ClinicalRequestState.COMPLETED:
+        await clinical_request.update(
+            current_state=data.new_state,
+            completed_at=datetime.now()
+        ).apply()
+    else:
+        await clinical_request.update(
+            current_state=data.new_state,
+            completed_at=datetime.now()
+        ).apply()
+    
     await pub.publish(
         'on-pathway-updated',
         await OnPathway.get(ClinicalRequest.on_pathway_id)
     )
+
     await pub.publish('clinical_request-resolutions', clinical_request)
+    
     return Response(status_code=200)
