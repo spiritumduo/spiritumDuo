6ced676fd77d79fa038e24a1ac84a4f0a5f765a4
Joe Channing <jcf@foxtrot-titan.co.uk>
2022-07-26 18:51:41 +0100

    Update decision point test

--- selenium_tests/tests/decision_point.feature
+++ selenium_tests/tests/decision_point.feature
@@ -1,8 +1,15 @@
 Feature: Decision points
-    Scenario: Opening a patient's decision point page
+    Scenario: Creating a decision point
         Given the user is logged in
         And the home page is displayed
         When I click on a patient row
         Then I should see the decision point modal
-        When I press the close button
+        Given I fill the text boxes
+        When I submit the form
+        Then I should see a confirmation of no requests selected
+        When I go back, select a request and resubmit
+        Then I should see a pre-submission confirmation window
+        When I submit the pre-submission confirmation
+        Then I should see the server confirmation window
+        When I submit the server confirmation
         Then it should close the modal
--- selenium_tests/tests/test_decision_point.py
+++ selenium_tests/tests/test_decision_point.py
@@ -3,10 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from pytest_bdd import scenario, given, when, then
 from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import (
+    expected_conditions as ExpectedConditions,
+)
 
 
-@scenario("decision_point.feature", "Opening a patient's decision point page")
-def test_show_decision_point_modal():
+@scenario(
+    "decision_point.feature",
+    "Creating a decision point"
+)
+def test_create_decision_point_with_requests():
     pass
 
 
@@ -17,7 +24,7 @@ def log_user_in(driver: webdriver.Remote, login_user: None):
 
 
 @given("the home page is displayed")
-def check_is_on_homepage(driver: webdriver.Remote, login_user: None):
+def check_is_on_homepage(driver: webdriver.Remote):
     assert_that(driver.current_url.endswith("/app/"), is_(equal_to(True)))
 
 
@@ -37,9 +44,79 @@ def check_modal_present(driver: webdriver.Remote):
     driver.find_element(By.CSS_SELECTOR, '.modal-body')
 
 
-@when("I press the close button")
-def close_modal(driver: webdriver.Remote):
-    driver.find_element(By.NAME, 'Close').click()
+@given("I fill the text boxes")
+def complete_form(driver: webdriver.Remote):
+    clinical_history = WebDriverWait(driver, 10).until(
+        lambda d: d.find_element(By.NAME, "clinicHistory")
+    )
+    clinical_history.send_keys("clinical_history")
+
+    comorbidities = WebDriverWait(driver, 10).until(
+        lambda d: d.find_element(By.NAME, "comorbidities")
+    )
+    comorbidities.send_keys("comorbidities")
+
+
+@when("I submit the form")
+def submit_form(driver: webdriver.Remote):
+    driver.find_element(
+        By.XPATH, "//button[contains(text(), 'Submit')]"
+    ).click()
+
+
+@then("I should see a confirmation of no requests selected")
+def check_no_request_confirmation(driver: webdriver.Remote):
+    driver.find_element(
+        By.XPATH,
+        "//h2[contains(text(), 'No requests selected!')]"
+    )
+
+
+@when("I go back, select a request and resubmit")
+def select_requests(driver: webdriver.Remote):
+    driver.find_element(
+        By.XPATH, "//button[contains(text(), 'Cancel')]"
+    ).click()
+
+    checkbox = driver.find_elements(By.XPATH, "//input[@type='checkbox']")[0]
+
+    WebDriverWait(driver, 10).until(
+        ExpectedConditions.element_to_be_clickable(
+            checkbox
+        )
+    )
+
+    checkbox.click()
+
+    driver.find_element(
+        By.XPATH, "//button[contains(text(), 'Submit')]"
+    ).click()
+
+
+@then("I should see a pre-submission confirmation window")
+def check_presub_confirmation(driver: webdriver.Remote):
+    driver.find_element(
+        By.XPATH,
+        "//h2[contains(text(), 'Submit these requests')]"
+    )
+
+
+@when("I submit the pre-submission confirmation")
+def submit_presub_confirmation(driver: webdriver.Remote):
+    driver.find_element(By.XPATH, "//button[contains(text(), 'OK')]").click()
+
+
+@then("I should see the server confirmation window")
+def check_server_confirmation(driver: webdriver.Remote):
+    driver.find_element(
+        By.XPATH,
+        "//h2[contains(text(), 'Decision Submitted')]"
+    )
+
+
+@when("I submit the server confirmation")
+def submit_server_confirmation(driver: webdriver.Remote):
+    driver.find_element(By.XPATH, "//button[contains(text(), 'OK')]").click()
 
 
 @then("it should close the modal")
