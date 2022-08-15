26313bd7643ae3c43cb72d7d152a3373caa42027
Joe Channing <jcf@foxtrot-titan.co.uk>
2022-08-15 17:23:44 +0100

    Linter fixes, docstrings

--- backend/src/datacreators/role.py
+++ backend/src/datacreators/role.py
@@ -2,5 +2,15 @@ from models import Role
 
 
 async def create_role(name: str):
-    return await Role.create(name=name)
+    """
+    Creates a role in the local database.
+    This is designed for the RESTful API.
 
+    Keyword arguments:
+        name (str): name of role to be created
+
+    Returns:
+        Role: new Role object
+
+    """
+    return await Role.create(name=name)
